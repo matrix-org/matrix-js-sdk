bd202b319382d58fd5f5d5e77a95ccc6b2469643
Kegan Dougal <kegan@matrix.org>
2015-06-03 16:22:04 +0100

    Remove browser-request dep from examples/browser.
    We no longer need this because the dist/ package includes a copy already. This
    simplifies browser development a lot.

--- examples/browser/browserTest.js
+++ examples/browser/browserTest.js
@@ -1,9 +1,6 @@
 "use strict";
 console.log("Loading browser sdk");
 
-// assign the global request module from browser-request.js
-matrixcs.request(request);
-
 var client = matrixcs.createClient("http://matrix.org");
 client.publicRooms(function (err, data) {
     if (err) {
