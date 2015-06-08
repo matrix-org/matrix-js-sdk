7ce3a781f3630eef8f2ee0942d4b4bed873ac8de
Kegan Dougal <kegan@matrix.org>
2015-06-08 11:47:15 +0100

    Add Room, RoomState and RoomMember classes.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 var matrixcs = require("./lib/matrix");
 matrixcs.request(require("request"));
-matrixcs.usePromises = function() {
-    matrixcs = require("./lib/matrix-promise");
-};
-
 module.exports = matrixcs;
