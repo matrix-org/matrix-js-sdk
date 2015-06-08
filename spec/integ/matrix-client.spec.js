a43447b00ee62aaaf6d0bb565a947e655f423fd6
Kegan Dougal <kegan@matrix.org>
2015-06-08 17:27:10 +0100

    Add tests to exercise room name / timeline logic; fix some bugs as a result.

--- spec/integ/matrix-client.spec.js
+++ spec/integ/matrix-client.spec.js
@@ -6,12 +6,19 @@ var utils = require("../test-utils");
 describe("MatrixClient", function() {
     var baseUrl = "http://localhost.or.something";
     var client, httpBackend;
+    var selfUserId = "@alice:localhost";
+    var selfAccessToken = "aseukfgwef";
+    var otherUserId = "@bob:localhost";
 
     beforeEach(function() {
         utils.beforeEach(this);
         httpBackend = new HttpBackend();
         sdk.request(httpBackend.requestFn);
-        client = sdk.createClient(baseUrl);
+        client = sdk.createClient({
+            baseUrl: baseUrl,
+            userId: selfUserId,
+            accessToken: selfAccessToken
+        });
     });
 
     afterEach(function() {
@@ -51,6 +58,121 @@ describe("MatrixClient", function() {
             client.startClient(function(err, data, isLive) {});
 
             httpBackend.flush().done(function() {
+                done();
+            });
+        });
+    });
+
+    describe("room state", function() {
+        var roomOne = "!foo:localhost";
+        var roomTwo = "!bar:localhost";
+        var msgText = "some text here";
+        var otherDisplayName = "Bob Smith";
+        var initialSync = {
+            end: "s_5_3",
+            presence: [],
+            rooms: [
+                {
+                    membership: "join",
+                    room_id: roomOne,
+                    messages: {
+                        start: "f_1_1",
+                        end: "f_2_2",
+                        chunk: [
+                            utils.mkMessage(roomOne, otherUserId, "hello")
+                        ]
+                    },
+                    state: [
+                        utils.mkEvent(
+                            "m.room.name", roomOne, otherUserId,
+                            {
+                                name: "Old room name"
+                            }
+                        ),
+                        utils.mkMembership(roomOne, "join", otherUserId),
+                        utils.mkMembership(roomOne, "join", selfUserId),
+                        utils.mkEvent(
+                            "m.room.create", roomOne, selfUserId,
+                            {
+                                creator: selfUserId
+                            }
+                        )
+                    ]
+                },
+                {
+                    membership: "join",
+                    room_id: roomTwo,
+                    messages: {
+                        start: "f_1_1",
+                        end: "f_2_2",
+                        chunk: [
+                            utils.mkMessage(roomTwo, otherUserId, "hiii")
+                        ]
+                    },
+                    state: [
+                        utils.mkMembership(
+                            roomTwo, "join", otherUserId, null, otherDisplayName
+                        ),
+                        utils.mkMembership(roomTwo, "join", selfUserId),
+                        utils.mkEvent(
+                            "m.room.create", roomTwo, selfUserId,
+                            {
+                                creator: selfUserId
+                            }
+                        )
+                    ]
+                }
+            ]
+        };
+        var eventData = {
+            start: "s_5_3",
+            end: "e_6_7",
+            chunk: [
+                utils.mkEvent("m.room.name", roomOne, selfUserId, {
+                    name: "A new room name"
+                }),
+                utils.mkMessage(roomTwo, otherUserId, msgText)
+            ]
+        };
+
+        it("should continually recalculate the right room name.", function(done) {
+            httpBackend.when("GET", "/initialSync").respond(200, initialSync);
+            httpBackend.when("GET", "/events").respond(200, eventData);
+
+            client.startClient(function(err, data, isLive) {});
+
+            httpBackend.flush().done(function() {
+                var room = client.getStore().getRoom(roomOne);
+                // should have clobbered the name to the one from /events
+                expect(room.name).toEqual(eventData.chunk[0].content.name);
+                done();
+            });
+        });
+
+        it("should store the right events in the timeline.", function(done) {
+            httpBackend.when("GET", "/initialSync").respond(200, initialSync);
+            httpBackend.when("GET", "/events").respond(200, eventData);
+
+            client.startClient(function(err, data, isLive) {});
+
+            httpBackend.flush().done(function() {
+                var room = client.getStore().getRoom(roomTwo);
+                // should have added the message from /events
+                expect(room.timeline.length).toEqual(2);
+                expect(room.timeline[1].getContent().body).toEqual(msgText);
+                done();
+            });
+        });
+
+        it("should set the right room name.", function(done) {
+            httpBackend.when("GET", "/initialSync").respond(200, initialSync);
+            httpBackend.when("GET", "/events").respond(200, eventData);
+
+            client.startClient(function(err, data, isLive) {});
+            httpBackend.flush().done(function() {
+                var room = client.getStore().getRoom(roomTwo);
+                // should use the display name of the other person.
+                expect(room.name).toEqual(otherDisplayName); 
                 done();
             });
         });
--- spec/test-utils.js
+++ spec/test-utils.js
@@ -10,3 +10,50 @@ module.exports.beforeEach = function(testCase) {
     console.log(desc);
     console.log(new Array(1 + desc.length).join("="));
 };
+
+/**
+ * Create a JSON object representing an Event.
+ * @param {string} type The event.type
+ * @param {string} room The event.room_id
+ * @param {string} userId The event.user_id
+ * @param {Object} content The event.content
+ */
+module.exports.mkEvent = function(type, room, userId, content) {
+    var event = {
+        type: type,
+        room_id: room,
+        user_id: userId,
+        content: content,
+        event_id: "$" + Math.random() + "-" + Math.random()
+    };
+    if (["m.room.name", "m.room.topic", "m.room.create", "m.room.join_rules",
+         "m.room.power_levels", "m.room.topic", "com.example.state"].indexOf(type)
+            !== -1) {
+        event.state_key = "";
+    }
+    return event;
+};
+
+module.exports.mkMembership = function(room, membership, userId, otherUserId,
+                                       displayName, avatarUrl) {
+    var event = module.exports.mkEvent("m.room.member", room, userId, {
+        membership: membership,
+        displayname: displayName,
+        avatar_url: avatarUrl
+    });
+    event.state_key = userId;
+    if (["invite", "ban"].indexOf(membership) !== -1) {
+        event.state_key = otherUserId;
+    }
+    return event;
+};
+
+module.exports.mkMessage = function(room, userId, msg) {
+    if (!msg) {
+        msg = "Random->"+Math.random();
+    }
+    return module.exports.mkEvent("m.room.message", room, userId, {
+        msgtype: "m.text",
+        body: msg
+    });
+};
