79c10c1b68b64c72bdc22b0a9b4babf3803c101e
Richard van der Hoff <richard@matrix.org>
2016-02-25 17:23:54 +0000

    Remove old tarball before building new one

--- jenkins.sh
+++ jenkins.sh
@@ -6,4 +6,8 @@ npm install
 npm test
 jshint --reporter=checkstyle -c .jshint lib spec > jshint.xml || echo "jshint finished with return code $?"
 gjslint --unix_mode --disable 0131,0211,0200,0222,0212 --max_line_length 90 -r lib/ -r spec/ > gjslint.log || echo "gjslint finished with return code $?"
+
+# delete the old tarball, if it exists
+rm -f matrix-js-sdk-*.tgz
+
 npm pack
