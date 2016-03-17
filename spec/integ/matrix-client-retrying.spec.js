02be0f659a28a3aa3ee03ee7bfc97d1c91316f98
Richard van der Hoff <richard@matrix.org>
2016-03-17 20:34:58 +0000

    Support for cancelling pending events
    Implement client.cancelPendingEvent which will cancel queued or not_sent events

--- spec/integ/matrix-client-retrying.spec.js
+++ spec/integ/matrix-client-retrying.spec.js
@@ -2,22 +2,30 @@
 var sdk = require("../..");
 var HttpBackend = require("../mock-request");
 var utils = require("../test-utils");
+var EventStatus = sdk.EventStatus;
 
 describe("MatrixClient retrying", function() {
     var baseUrl = "http://localhost.or.something";
     var client, httpBackend;
+    var scheduler;
     var userId = "@alice:localhost";
     var accessToken = "aseukfgwef";
+    var roomId = "!room:here";
+    var room;
 
     beforeEach(function() {
         utils.beforeEach(this);
         httpBackend = new HttpBackend();
         sdk.request(httpBackend.requestFn);
+        scheduler = new sdk.MatrixScheduler();
         client = sdk.createClient({
             baseUrl: baseUrl,
             userId: userId,
-            accessToken: accessToken
+            accessToken: accessToken,
+            scheduler: scheduler,
         });
+        room = new sdk.Room(roomId);
+        client.store.storeRoom(room);
     });
 
     afterEach(function() {
@@ -38,6 +46,49 @@ describe("MatrixClient retrying", function() {
 
     xit("should mark events as EventStatus.QUEUED when queued", function() {
 
+    });
+
+    it("should mark events as EventStatus.CANCELLED when cancelled", function(done) {
+
+        // send a couple of events; the second will be queued
+        var ev1, ev2;
+        client.sendMessage(roomId, "m1").then(function(ev) {
+            expect(ev).toEqual(ev1);
+        });
+        client.sendMessage(roomId, "m2").then(function(ev) {
+            expect(ev).toEqual(ev2);
+        });
+
+        // both events should be in the timeline at this point
+        var tl = room.getLiveTimeline().getEvents();
+        expect(tl.length).toEqual(2);
+        ev1 = tl[0];
+        ev2 = tl[1];
+
+        expect(ev1.status).toEqual(EventStatus.SENDING);
+        expect(ev2.status).toEqual(EventStatus.QUEUED);
+
+        // now we can cancel the second and check everything looks sane
+        client.cancelPendingEvent(ev2);
+        expect(ev2.status).toEqual(EventStatus.CANCELLED);
+        expect(tl.length).toEqual(1);
+
+        // shouldn't be able to cancel the first message yet
+        expect(function() { client.cancelPendingEvent(ev1); })
+            .toThrow();
+
+        // fail the first send
+        httpBackend.when("PUT", "/send/m.room.message/")
+            .respond(400);
+        httpBackend.flush().then(function() {
+            expect(ev1.status).toEqual(EventStatus.NOT_SENT);
+            expect(tl.length).toEqual(1);
+
+            // cancel the first message
+            client.cancelPendingEvent(ev1);
+            expect(ev1.status).toEqual(EventStatus.CANCELLED);
+            expect(tl.length).toEqual(0);
+        }).catch(utils.failTest).done(done);
     });
 
     describe("resending", function() {
