a47e59f02c458716d420d730283f277baa2e730a
Richard van der Hoff <richard@matrix.org>
2016-07-28 14:18:34 +0100

    make jenkins fail the build if the tests fail

--- jenkins.sh
+++ jenkins.sh
@@ -1,13 +1,31 @@
 #!/bin/bash -l
+
 export NVM_DIR="/home/jenkins/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm use 0.10
 npm install
-npm test
-jshint --reporter=checkstyle -c .jshint lib spec > jshint.xml || echo "jshint finished with return code $?"
-gjslint --unix_mode --disable 0131,0211,0200,0222,0212 --max_line_length 90 -r lib/ -r spec/ > gjslint.log || echo "gjslint finished with return code $?"
+
+RC=0
+
+function fail {
+    echo $@ >&2
+    RC=1
+}
+
+npm test || fail "npm test finished with return code $?"
+
+jshint --reporter=checkstyle -c .jshint lib spec > jshint.xml ||
+    fail "jshint finished with return code $?"
+
+gjslint --unix_mode --disable 0131,0211,0200,0222,0212 \
+        --max_line_length 90 \
+        -r lib/ -r spec/ > gjslint.log ||
+    fail "gjslint finished with return code $?"
 
 # delete the old tarball, if it exists
 rm -f matrix-js-sdk-*.tgz
 
-npm pack
+npm pack ||
+    fail "npm pack finished with return code $?"
+
+exit $RC
