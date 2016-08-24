matrix-org/matrix-js-sdk

commit 4cde51b3ce4dbb0d4ca5375192e63e8bf350aebc

Make DeviceInfo more useful, and refactor crypto methods to use it
This is a prerequisite for a forthcoming refactor of _encryptMessage out to a
separate class.

spec/integ/matrix-client-crypto.spec.js
@@ -175,6 +175,7 @@ function aliDownloadsKeys() {
             key: bobDeviceEd25519Key,
             verified: false,
             blocked: false,
+            display_name: null,
         }]);
     });
     var p2 = aliQueryKeys();

lib/crypto.js
@@ -15,9 +15,8 @@ limitations under the License.
 */
 "use strict";
 
+
 /**
- * Internal module
- *
  * @module crypto
  */
 
@@ -36,24 +35,92 @@ var DeviceVerification = {
 };
 
 /**
-  * Stored information about a user's device
+  * Information about a user's device
   *
-  * @typedef {Object} DeviceInfo
+  * @constructor
   *
-  * @property {string[]} altorithms list of algorithms supported by this device
+  * @property {string} deviceId the ID of this device
   *
-  * @property {Object} keys a map from &lt;key type&gt;:&lt;id&gt; -> key
+  * @property {string[]} algorithms list of algorithms supported by this device
+  *
+  * @property {Object.<string,string>} keys a map from
+  *      &lt;key type&gt;:&lt;id&gt; -> &lt;base64-encoded key&gt;>
   *
   * @property {DeviceVerification} verified whether the device has been
   *     verified by the user
   *
   * @property {Object} unsigned  additional data from the homeserver
+  *
+  * @param {string} deviceId id of the device
   */
+function DeviceInfo(deviceId) {
+    // you can't change the deviceId
+    Object.defineProperty(this, 'deviceId', {
+        enumerable: true,
+        value: deviceId,
+    });
+
+    this.algorithms = [];
+    this.keys = {};
+    this.verified = DeviceVerification.UNVERIFIED;
+    this.unsigned = {};
+}
+
+/**
+ * rehydrate a DeviceInfo from the session store
+ *
+ * @param {object} obj  raw object from session store
+ * @param {string} deviceId id of the device
+ *
+ * @return {module:crypto~DeviceInfo} new DeviceInfo
+ */
+DeviceInfo.fromStorage = function(obj, deviceId) {
+    var res = new DeviceInfo(deviceId);
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            res[prop] = obj[prop];
+        }
+    }
+    return res;
+};
+
+/**
+ * Prepare a DeviceInfo for JSON serialisation in the session store
+ *
+ * @return {object} deviceinfo with non-serialised members removed
+ */
+DeviceInfo.prototype.toStorage = function() {
+    return {
+        algorithms: this.algorithms,
+        keys: this.keys,
+        verified: this.verified,
+        unsigned: this.unsigned,
+    };
+};
+
+/**
+ * Get the fingerprint for this device (ie, the Ed25519 key)
+ *
+ * @return {string} base64-encoded fingerprint of this device
+ */
+DeviceInfo.prototype.getFingerprint = function() {
+    return this.keys["ed25519:" + this.deviceId];
+};
+
+/**
+ * Get the configured display name for this device, if any
+ *
+ * @return {string?} displayname
+ */
+DeviceInfo.prototype.getDisplayname = function() {
+    return this.unsigned.device_display_name || null;
+};
 
 /**
  * Cryptography bits
  *
  * @constructor
+ * @alias module:crypto
  *
  * @param {module:base-apis~MatrixBaseApis} baseApis base matrix api interface
  *
@@ -191,18 +258,26 @@ function _uploadOneTimeKeys(crypto) {
  */
 Crypto.prototype.downloadKeys = function(userIds, forceDownload) {
     var self = this;
+
+    // map from userid -> deviceid -> DeviceInfo
     var stored = {};
+
+    // list of userids we need to download keys for
     var downloadUsers = [];
 
     for (var i = 0; i < userIds.length; ++i) {
         var userId = userIds[i];
-        var devices = this._sessionStore.getEndToEndDevicesForUser(userId);
+        stored[userId] = {};
 
-        stored[userId] = devices || {};
-        if (devices && !forceDownload) {
-            continue;
+        var devices = this.getStoredDevicesForUser(userId);
+        for (var j = 0; j < devices.length; ++j) {
+            var dev = devices[j];
+            stored[userId][dev.deviceId] = dev;
         }
-        downloadUsers.push(userId);
+
+        if (devices.length === 0 || forceDownload) {
+            downloadUsers.push(userId);
+        }
     }
 
     if (downloadUsers.length === 0) {
@@ -218,14 +293,26 @@ Crypto.prototype.downloadKeys = function(userIds, forceDownload) {
                 continue;
             }
 
+            // map from deviceid -> deviceinfo for this user
             var userStore = stored[userId];
             var updated = _updateStoredDeviceKeysForUser(
                 self._olmDevice, userId, userStore, res.device_keys[userId]
             );
 
-            if (updated) {
+            if (!updated) {
+                continue;
+            }
+
+            // update the session store
+            var storage = {};
+            for (var deviceId in userStore) {
+                if (!userStore.hasOwnProperty(deviceId)) {
+                    continue;
+                }
+
+                storage[deviceId] = userStore[deviceId].toStorage();
                 self._sessionStore.storeEndToEndDevicesForUser(
-                    userId, userStore
+                    userId, storage
                 );
             }
         }
@@ -295,7 +382,7 @@ function _storeDeviceKeys(_olmDevice, userId, deviceId, userStore, deviceResult)
         return false;
     }
 
-    // prepare the canonical json: remove 'unsigned' and sigxsnatures, and
+    // prepare the canonical json: remove 'unsigned' and signatures, and
     // stringify with anotherjson
     delete deviceResult.unsigned;
     delete deviceResult.signatures;
@@ -310,12 +397,14 @@ function _storeDeviceKeys(_olmDevice, userId, deviceId, userStore, deviceResult)
         return false;
     }
 
+    // DeviceInfo
     var deviceStore;
+
     if (deviceId in userStore) {
         // already have this device.
         deviceStore = userStore[deviceId];
 
-        if (deviceStore.keys["ed25519:" + deviceId] != signKey) {
+        if (deviceStore.getFingerprint() != signKey) {
             // this should only happen if the list has been MITMed; we are
             // best off sticking with the original keys.
             //
@@ -325,9 +414,7 @@ function _storeDeviceKeys(_olmDevice, userId, deviceId, userStore, deviceResult)
             return false;
         }
     } else {
-        userStore[deviceId] = deviceStore = {
-            verified: DeviceVerification.UNVERIFIED
-        };
+        userStore[deviceId] = deviceStore = new DeviceInfo(deviceId);
     }
 
     deviceStore.keys = deviceResult.keys;
@@ -338,7 +425,31 @@ function _storeDeviceKeys(_olmDevice, userId, deviceId, userStore, deviceResult)
 
 
 /**
+ * Get the stored device keys for a user id
+ *
+ * @param {string} userId the user to list keys for.
+ *
+ * @return {module:crypto~DeviceInfo[]} list of devices
+ */
+Crypto.prototype.getStoredDevicesForUser = function(userId) {
+    var devs = this._sessionStore.getEndToEndDevicesForUser(userId);
+    if (!devs) {
+        return [];
+    }
+    var res = [];
+    for (var deviceId in devs) {
+        if (devs.hasOwnProperty(deviceId)) {
+            res.push(DeviceInfo.fromStorage(devs[deviceId], deviceId));
+        }
+    }
+    return res;
+};
+
+
+/**
  * List the stored device keys for a user id
+ *
+ * @deprecated prefer {@link module:crypto#getStoredDevicesForUser}
  *
  * @param {string} userId the user to list keys for.
  *
@@ -346,33 +457,31 @@ function _storeDeviceKeys(_olmDevice, userId, deviceId, userStore, deviceResult)
  *    "key", and "display_name" parameters.
  */
 Crypto.prototype.listDeviceKeys = function(userId) {
-    var devices = this._sessionStore.getEndToEndDevicesForUser(userId);
+    var devices = this.getStoredDevicesForUser(userId);
+
     var result = [];
-    if (devices) {
-        var deviceId;
-        var deviceIds = [];
-        for (deviceId in devices) {
-            if (devices.hasOwnProperty(deviceId)) {
-                deviceIds.push(deviceId);
-            }
-        }
-        deviceIds.sort();
-        for (var i = 0; i < deviceIds.length; ++i) {
-            deviceId = deviceIds[i];
-            var device = devices[deviceId];
-            var ed25519Key = device.keys["ed25519:" + deviceId];
-            var unsigned = device.unsigned || {};
-            if (ed25519Key) {
-                result.push({
-                    id: deviceId,
-                    key: ed25519Key,
-                    verified: Boolean(device.verified == DeviceVerification.VERIFIED),
-                    blocked: Boolean(device.verified == DeviceVerification.BLOCKED),
-                    display_name: unsigned.device_display_name,
-                });
-            }
+
+    for (var i = 0; i < devices.length; ++i) {
+        var device = devices[i];
+        var ed25519Key = device.getFingerprint();
+        if (ed25519Key) {
+            result.push({
+                id: device.deviceId,
+                key: ed25519Key,
+                verified: Boolean(device.verified == DeviceVerification.VERIFIED),
+                blocked: Boolean(device.verified == DeviceVerification.BLOCKED),
+                display_name: device.getDisplayname(),
+            });
         }
     }
+
+    // sort by deviceid
+    result.sort(function(a, b) {
+        if (a.deviceId < b.deviceId) { return -1; }
+        if (a.deviceId > b.deviceId) { return 1; }
+        return 0;
+    });
+
     return result;
 };
 
@@ -411,7 +520,7 @@ Crypto.prototype.getDeviceByIdentityKey = function(userId, algorithm, sender_key
             }
             var deviceKey = device.keys[keyId];
             if (deviceKey == sender_key) {
-                return device;
+                return DeviceInfo.fromStorage(device, deviceId);
             }
         }
     }
@@ -670,18 +779,16 @@ Crypto.prototype._encryptMessage = function(room, e2eRoomInfo, eventType, conten
     var participantKeys = [];
     for (var i = 0; i < users.length; ++i) {
         var userId = users[i];
-        var devices = this._sessionStore.getEndToEndDevicesForUser(userId);
-        for (var deviceId in devices) {
-            if (devices.hasOwnProperty(deviceId)) {
-                var dev = devices[deviceId];
-                if (dev.verified === DeviceVerification.BLOCKED) {
-                    continue;
-                }
+        var devices = this.getStoredDevicesForUser(userId);
+        for (var j = 0; j < devices.length; ++j) {
+            var dev = devices[j];
+            if (dev.blocked) {
+                continue;
+            }
 
-                for (var keyId in dev.keys) {
-                    if (keyId.indexOf("curve25519:") === 0) {
-                        participantKeys.push(dev.keys[keyId]);
-                    }
+            for (var keyId in dev.keys) {
+                if (keyId.indexOf("curve25519:") === 0) {
+                    participantKeys.push(dev.keys[keyId]);
                 }
             }
         }

spec/integ/matrix-client-methods.spec.js
@@ -236,27 +236,22 @@ describe("MatrixClient", function() {
             });
 
             client.downloadKeys(["boris", "chaz", "dave"]).then(function(res) {
-                expect(res).toEqual({
-                    boris: {
-                        dev1: {
-                            verified: 0, // DeviceVerification.UNVERIFIED
-                            keys: { "ed25519:dev1": ed25519key },
-                            algorithms: ["1"],
-                            unsigned: { "abc": "def" },
-                        },
-                    },
-                    chaz: {
-                        dev2: {
-                            verified: 0, // DeviceVerification.UNVERIFIED
-                            keys: { "ed25519:dev2" : ed25519key },
-                            algorithms: ["2"],
-                            unsigned: { "ghi": "def" },
-                        },
-                    },
-                    dave: {
-                        // dave's key fails validation.
-                    },
+                assertObjectContains(res.boris.dev1, {
+                    verified: 0, // DeviceVerification.UNVERIFIED
+                    keys: { "ed25519:dev1": ed25519key },
+                    algorithms: ["1"],
+                    unsigned: { "abc": "def" },
                 });
+
+                assertObjectContains(res.chaz.dev2, {
+                    verified: 0, // DeviceVerification.UNVERIFIED
+                    keys: { "ed25519:dev2" : ed25519key },
+                    algorithms: ["2"],
+                    unsigned: { "ghi": "def" },
+                });
+
+                // dave's key fails validation.
+                expect(res.dave).toEqual({});
             }).catch(utils.failTest).done(done);
 
             httpBackend.flush();
@@ -278,3 +273,11 @@ describe("MatrixClient", function() {
         });
     });
 });
+
+function assertObjectContains(obj, expected) {
+    for (var k in expected) {
+        if (expected.hasOwnProperty(k)) {
+            expect(obj[k]).toEqual(expected[k]);
+        }
+    }
+}

commit 31e7addf2fc65a5b9ff5fecf5657bcac2751bfc1

Reinstate device blocking for simple Olm
Commit 4cde51b3 broke device blocking such that we were encrypting for all
devices, including blocked ones. Reinstate it, and add a test.

lib/crypto-algorithms/olm.js
@@ -24,6 +24,9 @@ var q = require('q');
 
 var utils = require("../utils");
 var olmlib = require("../olmlib");
+var DeviceInfo = require("../crypto-deviceinfo");
+var DeviceVerification = DeviceInfo.DeviceVerification;
+
 
 var base = require("./base");
 
@@ -77,21 +80,17 @@ OlmEncryption.prototype.encryptMessage = function(room, eventType, content) {
         var userId = users[i];
         var devices = this._crypto.getStoredDevicesForUser(userId);
         for (var j = 0; j < devices.length; ++j) {
-            var dev = devices[j];
-            if (dev.blocked) {
+            var deviceInfo = devices[j];
+            var key = deviceInfo.getIdentityKey();
+            if (key == this._olmDevice.deviceCurve25519Key) {
+                // don't bother setting up session to ourself
                 continue;
             }
-
-            for (var keyId in dev.keys) {
-                if (keyId.indexOf("curve25519:") === 0) {
-                    var key = dev.keys[keyId];
-
-                    // don't send to ourselves.
-                    if (key != this._olmDevice.deviceCurve25519Key) {
-                        participantKeys.push(key);
-                    }
-                }
+            if (deviceInfo.verified == DeviceVerification.BLOCKED) {
+                // don't bother setting up sessions with blocked users
+                continue;
             }
+            participantKeys.push(key);
         }
     }
 

spec/integ/matrix-client-crypto.spec.js
@@ -2,7 +2,9 @@
 var sdk = require("../..");
 var q = require("q");
 var HttpBackend = require("../mock-request");
-var utils = require("../test-utils");
+var utils = require("../../lib/utils");
+var test_utils = require("../test-utils");
+
 function MockStorageApi() {
     this.data = {};
 }
@@ -119,7 +121,7 @@ function expectBobKeyUpload() {
 }
 
 function bobUploadsKeys() {
-    bobClient.uploadKeys(5).catch(utils.failTest);
+    bobClient.uploadKeys(5).catch(test_utils.failTest);
     return expectBobKeyUpload();
 }
 
@@ -194,7 +196,7 @@ function aliEnablesEncryption() {
     // can't query keys before bob has uploaded them
     expect(bobOneTimeKeys).toBeDefined();
 
-    aliQueryKeys().catch(utils.failTest);
+    aliQueryKeys().catch(test_utils.failTest);
     aliHttpBackend.when("POST", "/keys/claim").respond(200, function(path, content) {
         expect(content.one_time_keys[bobUserId][bobDeviceId]).toEqual("curve25519");
         for (var keyId in bobOneTimeKeys) {
@@ -223,7 +225,7 @@ function aliEnablesEncryption() {
 }
 
 function bobEnablesEncryption() {
-    bobQueryKeys().catch(utils.failTest);
+    bobQueryKeys().catch(test_utils.failTest);
     return bobClient.setRoomEncryption(roomId, {
         algorithm: "m.olm.v1.curve25519-aes-sha2",
     }).then(function(res) {
@@ -237,6 +239,7 @@ function bobEnablesEncryption() {
 function aliSendsMessage() {
     return sendMessage(aliHttpBackend, aliClient).then(function(content) {
         aliMessages.push(content);
+        expect(utils.keys(content.ciphertext)).toEqual([bobDeviceCurve25519Key]);
         var ciphertext = content.ciphertext[bobDeviceCurve25519Key];
         expect(ciphertext).toBeDefined();
     });
@@ -247,6 +250,7 @@ function bobSendsMessage() {
         bobMessages.push(content);
         var aliKeyId = "curve25519:" + aliDeviceId;
         var aliDeviceCurve25519Key = aliDeviceKeys.keys[aliKeyId];
+        expect(utils.keys(content.ciphertext)).toEqual([aliDeviceCurve25519Key]);
         var ciphertext = content.ciphertext[aliDeviceCurve25519Key];
         expect(ciphertext).toBeDefined();
         return ciphertext;
@@ -293,7 +297,7 @@ function recvMessage(httpBackend, client, message) {
     syncData.rooms.join[roomId] = {
         timeline: {
             events: [
-                utils.mkEvent({
+                test_utils.mkEvent({
                     type: "m.room.encrypted",
                     room: roomId,
                     content: message
@@ -331,7 +335,7 @@ function recvMessage(httpBackend, client, message) {
 
 
 function aliStartClient() {
-    expectAliKeyUpload().catch(utils.failTest);
+    expectAliKeyUpload().catch(test_utils.failTest);
     startClient(aliHttpBackend, aliClient);
     return aliHttpBackend.flush().then(function() {
         console.log("Ali client started");
@@ -339,7 +343,7 @@ function aliStartClient() {
 }
 
 function bobStartClient() {
-    expectBobKeyUpload().catch(utils.failTest);
+    expectBobKeyUpload().catch(test_utils.failTest);
     startClient(bobHttpBackend, bobClient);
     return bobHttpBackend.flush().then(function() {
         console.log("Bob client started");
@@ -368,11 +372,11 @@ function startClient(httpBackend, client) {
     syncData.rooms.join[roomId] = {
         state: {
             events: [
-                utils.mkMembership({
+                test_utils.mkMembership({
                     mship: "join",
                     user: aliUserId,
                 }),
-                utils.mkMembership({
+                test_utils.mkMembership({
                     mship: "join",
                     user: bobUserId,
                 }),
@@ -397,7 +401,7 @@ describe("MatrixClient crypto", function() {
         aliLocalStore = new MockStorageApi();
         aliStorage = new sdk.WebStorageSessionStore(aliLocalStore);
         bobStorage = new sdk.WebStorageSessionStore(new MockStorageApi());
-        utils.beforeEach(this);
+        test_utils.beforeEach(this);
 
         aliHttpBackend = new HttpBackend();
         aliClient = sdk.createClient({
@@ -436,14 +440,14 @@ describe("MatrixClient crypto", function() {
     it("Bob uploads without one-time keys and with one-time keys", function(done) {
         q()
             .then(bobUploadsKeys)
-            .catch(utils.failTest).done(done);
+            .catch(test_utils.failTest).done(done);
     });
 
     it("Ali downloads Bobs keys", function(done) {
         q()
             .then(bobUploadsKeys)
             .then(aliDownloadsKeys)
-            .catch(utils.failTest).done(done);
+            .catch(test_utils.failTest).done(done);
     });
 
     it("Ali gets keys with an invalid signature", function(done) {
@@ -461,7 +465,7 @@ describe("MatrixClient crypto", function() {
                 // should get an empty list
                 expect(aliClient.listDeviceKeys(bobUserId)).toEqual([]);
             })
-            .catch(utils.failTest).done(done);
+            .catch(test_utils.failTest).done(done);
     });
 
     it("Ali enables encryption", function(done) {
@@ -469,7 +473,7 @@ describe("MatrixClient crypto", function() {
             .then(bobUploadsKeys)
             .then(aliStartClient)
             .then(aliEnablesEncryption)
-            .catch(utils.failTest).done(done);
+            .catch(test_utils.failTest).done(done);
     });
 
     it("Ali sends a message", function(done) {
@@ -478,7 +482,7 @@ describe("MatrixClient crypto", function() {
             .then(aliStartClient)
             .then(aliEnablesEncryption)
             .then(aliSendsMessage)
-            .catch(utils.failTest).done(done);
+            .catch(test_utils.failTest).done(done);
     });
 
     it("Bob receives a message", function(done) {
@@ -489,7 +493,21 @@ describe("MatrixClient crypto", function() {
             .then(aliSendsMessage)
             .then(bobStartClient)
             .then(bobRecvMessage)
-            .catch(utils.failTest).done(done);
+            .catch(test_utils.failTest).done(done);
+    });
+
+    it("Ali blocks Bob's device", function(done) {
+        q()
+            .then(bobUploadsKeys)
+            .then(aliStartClient)
+            .then(aliEnablesEncryption)
+            .then(function() {
+                aliClient.setDeviceBlocked(bobUserId, bobDeviceId, true);
+                return sendMessage(aliHttpBackend, aliClient);
+            }).then(function(sentContent) {
+                // no unblocked devices, so the ciphertext should be empty
+                expect(sentContent.ciphertext).toEqual({});
+            }).catch(test_utils.failTest).done(done);
     });
 
     it("Bob receives two pre-key messages", function(done) {
@@ -502,7 +520,7 @@ describe("MatrixClient crypto", function() {
             .then(bobRecvMessage)
             .then(aliSendsMessage)
             .then(bobRecvMessage)
-            .catch(utils.failTest).done(done);
+            .catch(test_utils.failTest).done(done);
     });
 
     it("Bob replies to the message", function(done) {
@@ -517,6 +535,6 @@ describe("MatrixClient crypto", function() {
             .then(bobSendsMessage).then(function(ciphertext) {
                 expect(ciphertext.type).toEqual(1);
             }).then(aliRecvMessage)
-            .catch(utils.failTest).done(done);
+            .catch(test_utils.failTest).done(done);
     });
 });