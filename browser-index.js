18f57a210044bad6697f52df5a1461950d1b3e62
David Baker <dave@matrix.org>
2017-01-11 18:52:49 +0000

    Remove babel-polyfill
    react-sdk's tests are failing because babel-polyfill is being
    pulled in twice.
    
    As per https://babeljs.io/docs/usage/polyfill/ babel-polyfill is
    "intended to be used in an application rather than a library/tool".
    From a library, we're limited to things that don't modify globals,
    since the js env is not ours to start screwing around with.

--- browser-index.js
+++ browser-index.js
@@ -1,5 +1,3 @@
-require("babel-polyfill");
-
 var matrixcs = require("./lib/matrix");
 matrixcs.request(require("browser-request"));
 module.exports = matrixcs; // keep export for browserify package deps
