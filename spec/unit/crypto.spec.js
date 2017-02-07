d744cecbfa0ae64c4fddccae0dad7bbeaf2b81aa
Richard van der Hoff <richard@matrix.org>
2017-02-07 22:56:16 +0000

    Make the tests work without olm
    Olm is an optional dependency, so the tests should work without it.

--- spec/unit/crypto.spec.js
+++ spec/unit/crypto.spec.js
@@ -1,13 +1,16 @@
 
 "use strict";
-const Crypto = require("../../lib/crypto");
+
 const sdk = require("../..");
+let Crypto;
+if (sdk.CRYPTO_ENABLED) {
+    Crypto = require("../../lib/crypto");
+}
 
 describe("Crypto", function() {
     if (!sdk.CRYPTO_ENABLED) {
         return;
     }
-
     it("Crypto exposes the correct olm library version", function() {
         expect(Crypto.getOlmVersion()[0]).toEqual(2);
     });
