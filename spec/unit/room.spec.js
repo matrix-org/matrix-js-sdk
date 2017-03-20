dc8a2670ab87506b573c117d7de3cf53a179b597
Kegan Dougal <kegan@matrix.org>
2017-03-20 12:05:22 +0000

    Unbreak tests

--- spec/unit/room.spec.js
+++ spec/unit/room.spec.js
@@ -446,15 +446,14 @@ describe("Room", function() {
             expect(callCount).toEqual(1);
         });
 
-        it("should " + (timelineSupport ? "remember" : "forget") +
-                " old timelines", function() {
+        it("should forget old timelines", function() {
             room.addLiveEvents([events[0]]);
             expect(room.timeline.length).toEqual(1);
             const firstLiveTimeline = room.getLiveTimeline();
             room.resetLiveTimeline();
 
             const tl = room.getTimelineForEvent(events[0].getId());
-            expect(tl).toBe(timelineSupport ? firstLiveTimeline : null);
+            expect(tl).toBe(null);
         });
     };
 
