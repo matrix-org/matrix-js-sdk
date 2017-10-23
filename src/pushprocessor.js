e33b786f65fab3bb274aa9a962e55a4233566308
David Baker <dave@matrix.org>
2017-10-23 16:02:06 +0100

    Support room notifs
    Specifically, add support for the sender_notification_permission
    push rule condition. Also delays calculating notification events
    until after they're added to the timeline, as per comment.

--- src/pushprocessor.js
+++ src/pushprocessor.js
@@ -123,6 +123,7 @@ function PushProcessor(client) {
             "device": eventFulfillsDeviceCondition,
             "contains_display_name": eventFulfillsDisplayNameCondition,
             "room_member_count": eventFulfillsRoomMemberCountCondition,
+            "sender_notification_permission": eventFulfillsSenderNotificationPermCondition,
         };
         if (condition_functions[cond.kind]) {
             return condition_functions[cond.kind](cond, ev);
@@ -132,6 +133,33 @@ function PushProcessor(client) {
         // it's probably better to not match unknown conditions.
         return false;
     };
+
+    const eventFulfillsSenderNotificationPermCondition = function(cond, ev) {
+        const notifLevelKey = cond['key'];
+        if (!notifLevelKey) {
+            return false;
+        }
+
+        const room = client.getRoom(ev.getRoomId());
+        if (!room || !room.currentState) {
+            return false;
+        }
+
+        const powerLevels = room.currentState.getStateEvents('m.room.power_levels', '');
+        if (!powerLevels || !powerLevels.getContent()) {
+            return false;
+        }
+
+        let notifLevel = 50;
+        if (
+            powerLevels.getContent().notifications &&
+            powerLevels.getContent().notifications[notifLevelKey]
+        ) {
+            notifLevel = powerLevels.getContent().notifications[notifLevelKey];
+        }
+
+        return ev.sender.powerLevel >= notifLevel;
+    }
 
     const eventFulfillsRoomMemberCountCondition = function(cond, ev) {
         if (!cond.is) {
