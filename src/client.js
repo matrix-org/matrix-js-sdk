f1194b1fbeec1dce0fc6ca5fdbeb7a46a3fa5ee6
David Baker <dave@matrix.org>
2018-01-31 17:33:02 +0000

    more comment grammar

--- src/client.js
+++ src/client.js
@@ -184,7 +184,7 @@ function MatrixClient(opts) {
     }
 
     // List of which rooms have encryption enabled: separate from crypto because
-    // we still want to know what rooms are encrypted even if crypto is disabled:
+    // we still want to know which rooms are encrypted even if crypto is disabled:
     // we don't want to start sening unencrypted events to them.
     this._roomList = new RoomList(this._cryptoStore, this._sessionStore);
 }
