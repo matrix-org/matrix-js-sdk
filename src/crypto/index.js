74707c284f4b8f253a32ec1818274f86f6df7ef4
Michael Telatynski <7t3chguy@gmail.com>
2018-02-10 10:34:31 +0000

    if event has `m.relates_to` extract this into the e2e-wrapper event
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/crypto/index.js
+++ src/crypto/index.js
@@ -769,9 +769,21 @@ Crypto.prototype.encryptEvent = function(event, room) {
         );
     }
 
+    const content = event.getContent();
+    // If event has an m.relates_to then we need
+    // to put this on the wrapping event instead
+    const mRelatesTo = content['m.relates_to'];
+    if (mRelatesTo) {
+        delete content['m.relates_to'];
+    }
+
     return alg.encryptMessage(
-        room, event.getType(), event.getContent(),
+        room, event.getType(), content,
     ).then((encryptedContent) => {
+        if (mRelatesTo) {
+            encryptedContent['m.relates_to'] = mRelatesTo;
+        }
+
         event.makeEncrypted(
             "m.room.encrypted",
             encryptedContent,
