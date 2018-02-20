8aa68b3dc1124685b61cd9bb1feb0d5c796bc062
David Baker <dave@matrix.org>
2018-02-20 11:09:32 +0000

    lint

--- spec/integ/matrix-client-syncing.spec.js
+++ spec/integ/matrix-client-syncing.spec.js
@@ -447,11 +447,15 @@ describe("MatrixClient syncing", function() {
                 awaitSyncEvent(2),
             ]).then(function() {
                 const room = client.getRoom(roomOne);
-                const stateAtStart = room.getLiveTimeline().getState(EventTimeline.BACKWARDS);
+                const stateAtStart = room.getLiveTimeline().getState(
+                    EventTimeline.BACKWARDS,
+                );
                 const startRoomNameEvent = stateAtStart.getStateEvents('m.room.name', '');
                 expect(startRoomNameEvent.getContent().name).toEqual('Old room name');
 
-                const stateAtEnd = room.getLiveTimeline().getState(EventTimeline.FORWARDS);
+                const stateAtEnd = room.getLiveTimeline().getState(
+                    EventTimeline.FORWARDS,
+                );
                 const endRoomNameEvent = stateAtEnd.getStateEvents('m.room.name', '');
                 expect(endRoomNameEvent.getContent().name).toEqual('A new room name');
             });
