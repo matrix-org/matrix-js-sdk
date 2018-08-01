c84a35bd4b8c35bd441312d3499c317ac7d5a10e
Bruno Windels <brunow@matrix.org>
2018-08-01 17:24:36 +0200

    prepend state from /messages to appropriate timeline/room state (excluding event context for now)

--- src/client.js
+++ src/client.js
@@ -1954,6 +1954,10 @@ MatrixClient.prototype.scrollback = function(room, limit, callback) {
             'b');
     }).done(function(res) {
         const matrixEvents = utils.map(res.chunk, _PojoToMatrixEventMapper(self));
+        if (res.state) {
+            const stateEvents = utils.map(res.state, _PojoToMatrixEventMapper(self));
+            room.currentState.prependStateEvents(stateEvents);
+        }
         room.addEventsToTimeline(matrixEvents, true, room.getLiveTimeline());
         room.oldState.paginationToken = res.end;
         if (res.chunk.length === 0) {
@@ -2022,6 +2026,7 @@ MatrixClient.prototype.paginateEventContext = function(eventContext, opts) {
 
     const self = this;
     promise.then(function(res) {
+        // TODO: no RoomState on EventContext, where should the members go?
         let token = res.end;
         if (res.chunk.length === 0) {
             token = null;
@@ -2252,6 +2257,11 @@ MatrixClient.prototype.paginateEventTimeline = function(eventTimeline, opts) {
             dir,
             eventTimeline.getFilter());
         promise.then(function(res) {
+            if (res.state) {
+                const roomState = eventTimeline.getState(dir);
+                const stateEvents = utils.map(res.state, self.getEventMapper());
+                roomState.prependStateEvents(stateEvents);
+            }
             const token = res.end;
             const matrixEvents = utils.map(res.chunk, self.getEventMapper());
             eventTimeline.getTimelineSet()
