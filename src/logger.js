ae645ad9f0173639dfaf642928684849e4a16ec1
André Jaenisch <andre.jaenisch@posteo.de>
2018-10-24 21:52:50 +0200

    Use Node.js module export, since ES6 export breaks build.
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/logger.js
+++ src/logger.js
@@ -33,4 +33,4 @@ logger.setLevel(log.levels.DEBUG);
  * Drop-in replacement for <code>console</code> using {@link https://www.npmjs.com/package/loglevel|loglevel}.
  * Can be tailored down to specific use cases if needed.
 */
-export default logger;
+module.exports = logger;
