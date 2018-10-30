054aac17aa8870157a6ea2d740fca9aef6ef2af7
Travis Ralston <travpc@gmail.com>
2018-10-30 14:25:25 -0600

    Just use the interface provided to us rather than hack in a flag

--- browser-index.js
+++ browser-index.js
@@ -1,7 +1,17 @@
 var matrixcs = require("./lib/matrix");
-var request = require("browser-request");
-global.enableConstructionOfQueryString = true; // note: this is long so we hopefully don't collide
-matrixcs.request(request);
+const request = require('browser-request');
+const queryString = require('query-string');
+
+matrixcs.request((opts, fn) => {
+    // We manually fix the query string for browser-request because
+    // it doesn't correctly handle cases like ?via=one&via=two. Instead
+    // we mimic `request`'s query string interface to make it all work
+    // as expected.
+    // browser-request will happily take the constructed string as the
+    // query string without trying to modify it further.
+    opts.qs = queryString.stringify(opts.qs || {}, opts.qsStringifyOptions);
+    return request(opts, fn);
+});
 
 // just *accessing* indexedDB throws an exception in firefox with
 // indexeddb disabled.
--- src/http-api.js
+++ src/http-api.js
@@ -19,7 +19,6 @@ limitations under the License.
  * @module http-api
  */
 import Promise from 'bluebird';
-const queryString = require('query-string');
 const parseContentType = require('content-type').parse;
 
 const utils = require("./utils");
@@ -747,9 +746,6 @@ module.exports.MatrixHttpApi.prototype = {
         const reqPromise = defer.promise;
 
         try {
-            if (global && global.enableConstructionOfQueryString) {
-                queryParams = queryString.stringify(queryParams, opts.qsStringifyOptions);
-            }
             req = this.opts.request(
                 {
                     uri: uri,
