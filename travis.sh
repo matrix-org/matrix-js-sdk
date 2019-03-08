cc86f427d226241c7b5a6bde0346524faefca3fd
J. Ryan Stinnett <jryans@gmail.com>
2019-03-08 15:58:08 +0000

    Convert Olm to a dev dependency
    This gives more natural behavior of downloading Olm by default when working on
    this module, so that all tests including crypto are run.

--- travis.sh
+++ travis.sh
@@ -4,13 +4,6 @@ set -ex
 
 yarn lint
 
-# install Olm so that we can run the crypto tests.
-# This will add Olm as dependency, since it's currently unlisted.
-# (`yarn` does not have an install dependency without adding mode.)
-# TODO: Should Olm be a listed dev dependency instead, so that we can have it for testing
-# and don't need to run an extra step here?
-yarn add https://matrix.org/packages/npm/olm/olm-3.1.0-pre1.tgz
-
 yarn test
 
 yarn gendoc
