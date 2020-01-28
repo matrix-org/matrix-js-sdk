f410c2f59efb9344f7e0aa07fb1f65e5fc924e0a
Jack Works <jackworks@protonmail.com>
2020-01-28 22:58:21 +0800

    refactor: refactor indexeddb to es6 class
    Signed-off-by: Jack Works <jackworks@protonmail.com>

--- src/store/indexeddb.js
+++ src/store/indexeddb.js
@@ -18,14 +18,14 @@ limitations under the License.
 
 /* eslint-disable babel/no-invalid-this */
 
-import {MemoryStore} from "./memory";
+import { MemoryStore } from "./memory";
 import * as utils from "../utils";
-import {EventEmitter} from 'events';
-import {LocalIndexedDBStoreBackend} from "./indexeddb-local-backend.js";
-import {RemoteIndexedDBStoreBackend} from "./indexeddb-remote-backend.js";
-import {User} from "../models/user";
-import {MatrixEvent} from "../models/event";
-import {logger} from '../logger';
+import { EventEmitter } from 'events';
+import { LocalIndexedDBStoreBackend } from "./indexeddb-local-backend.js";
+import { RemoteIndexedDBStoreBackend } from "./indexeddb-remote-backend.js";
+import { User } from "../models/user";
+import { MatrixEvent } from "../models/event";
+import { logger } from '../logger';
 
 /**
  * This is an internal module. See {@link IndexedDBStore} for the public class.
@@ -82,198 +82,199 @@ const WRITE_DELAY_MS = 1000 * 60 * 5; // once every 5 minutes
  * this API if you need to perform specific indexeddb actions like deleting the
  * database.
  */
-export function IndexedDBStore(opts) {
-    MemoryStore.call(this, opts);
+export class IndexedDBStore extends MemoryStore {
+    constructor(opts) {
+        super(opts);
 
-    if (!opts.indexedDB) {
-        throw new Error('Missing required option: indexedDB');
-    }
-
-    if (opts.workerScript) {
-        // try & find a webworker-compatible API
-        let workerApi = opts.workerApi;
-        if (!workerApi) {
-            // default to the global Worker object (which is where it in a browser)
-            workerApi = global.Worker;
+        if (!opts.indexedDB) {
+            throw new Error('Missing required option: indexedDB');
         }
-        this.backend = new RemoteIndexedDBStoreBackend(
-            opts.workerScript, opts.dbName, workerApi,
-        );
-    } else {
-        this.backend = new LocalIndexedDBStoreBackend(opts.indexedDB, opts.dbName);
-    }
 
-    this.startedUp = false;
-    this._syncTs = 0;
-
-    // Records the last-modified-time of each user at the last point we saved
-    // the database, such that we can derive the set if users that have been
-    // modified since we last saved.
-    this._userModifiedMap = {
-        // user_id : timestamp
-    };
-}
-utils.inherits(IndexedDBStore, MemoryStore);
-utils.extend(IndexedDBStore.prototype, EventEmitter.prototype);
-
-IndexedDBStore.exists = function(indexedDB, dbName) {
-    return LocalIndexedDBStoreBackend.exists(indexedDB, dbName);
-};
-
-/**
- * @return {Promise} Resolved when loaded from indexed db.
-  */
-IndexedDBStore.prototype.startup = function() {
-    if (this.startedUp) {
-        logger.log(`IndexedDBStore.startup: already started`);
-        return Promise.resolve();
-    }
-
-    logger.log(`IndexedDBStore.startup: connecting to backend`);
-    return this.backend.connect().then(() => {
-        logger.log(`IndexedDBStore.startup: loading presence events`);
-        return this.backend.getUserPresenceEvents();
-    }).then((userPresenceEvents) => {
-        logger.log(`IndexedDBStore.startup: processing presence events`);
-        userPresenceEvents.forEach(([userId, rawEvent]) => {
-            const u = new User(userId);
-            if (rawEvent) {
-                u.setPresenceEvent(new MatrixEvent(rawEvent));
+        if (opts.workerScript) {
+            // try & find a webworker-compatible API
+            let workerApi = opts.workerApi;
+            if (!workerApi) {
+                // default to the global Worker object (which is where it in a browser)
+                workerApi = global.Worker;
             }
-            this._userModifiedMap[u.userId] = u.getLastModifiedTime();
-            this.storeUser(u);
+            this.backend = new RemoteIndexedDBStoreBackend(
+                opts.workerScript, opts.dbName, workerApi,
+            );
+        } else {
+            this.backend = new LocalIndexedDBStoreBackend(opts.indexedDB, opts.dbName);
+        }
+
+        this.startedUp = false;
+        this._syncTs = 0;
+
+        // Records the last-modified-time of each user at the last point we saved
+        // the database, such that we can derive the set if users that have been
+        // modified since we last saved.
+        this._userModifiedMap = {
+            // user_id : timestamp
+        };
+    }
+
+    static exists = function (indexedDB, dbName) {
+        return LocalIndexedDBStoreBackend.exists(indexedDB, dbName);
+    };
+
+    /**
+     * @return {Promise} Resolved when loaded from indexed db.
+      */
+    startup() {
+        if (this.startedUp) {
+            logger.log(`IndexedDBStore.startup: already started`);
+            return Promise.resolve();
+        }
+
+        logger.log(`IndexedDBStore.startup: connecting to backend`);
+        return this.backend.connect().then(() => {
+            logger.log(`IndexedDBStore.startup: loading presence events`);
+            return this.backend.getUserPresenceEvents();
+        }).then((userPresenceEvents) => {
+            logger.log(`IndexedDBStore.startup: processing presence events`);
+            userPresenceEvents.forEach(([userId, rawEvent]) => {
+                const u = new User(userId);
+                if (rawEvent) {
+                    u.setPresenceEvent(new MatrixEvent(rawEvent));
+                }
+                this._userModifiedMap[u.userId] = u.getLastModifiedTime();
+                this.storeUser(u);
+            });
+        });
+    };
+
+    /**
+     * @return {Promise} Resolves with a sync response to restore the
+     * client state to where it was at the last save, or null if there
+     * is no saved sync data.
+     */
+    getSavedSync = degradable(function () {
+        return this.backend.getSavedSync();
+    }, "getSavedSync");
+
+    /** @return {Promise<bool>} whether or not the database was newly created in this session. */
+    isNewlyCreated = degradable(function () {
+        return this.backend.isNewlyCreated();
+    }, "isNewlyCreated");
+
+    /**
+     * @return {Promise} If there is a saved sync, the nextBatch token
+     * for this sync, otherwise null.
+     */
+    getSavedSyncToken = degradable(function () {
+        return this.backend.getNextBatchToken();
+    }, "getSavedSyncToken"),
+
+    /**
+     * Delete all data from this store.
+     * @return {Promise} Resolves if the data was deleted from the database.
+     */
+    deleteAllData = degradable(function () {
+        MemoryStore.prototype.deleteAllData.call(this);
+        return this.backend.clearDatabase().then(() => {
+            logger.log("Deleted indexeddb data.");
+        }, (err) => {
+            logger.error(`Failed to delete indexeddb data: ${err}`);
+            throw err;
         });
     });
-};
 
-/**
- * @return {Promise} Resolves with a sync response to restore the
- * client state to where it was at the last save, or null if there
- * is no saved sync data.
- */
-IndexedDBStore.prototype.getSavedSync = degradable(function() {
-    return this.backend.getSavedSync();
-}, "getSavedSync");
+    /**
+     * Whether this store would like to save its data
+     * Note that obviously whether the store wants to save or
+     * not could change between calling this function and calling
+     * save().
+     *
+     * @return {boolean} True if calling save() will actually save
+     *     (at the time this function is called).
+     */
+    wantsSave() {
+        const now = Date.now();
+        return now - this._syncTs > WRITE_DELAY_MS;
+    };
 
-/** @return {Promise<bool>} whether or not the database was newly created in this session. */
-IndexedDBStore.prototype.isNewlyCreated = degradable(function() {
-    return this.backend.isNewlyCreated();
-}, "isNewlyCreated");
+    /**
+     * Possibly write data to the database.
+     *
+     * @param {bool} force True to force a save to happen
+     * @return {Promise} Promise resolves after the write completes
+     *     (or immediately if no write is performed)
+     */
+    save(force) {
+        if (force || this.wantsSave()) {
+            return this._reallySave();
+        }
+        return Promise.resolve();
+    };
 
-/**
- * @return {Promise} If there is a saved sync, the nextBatch token
- * for this sync, otherwise null.
- */
-IndexedDBStore.prototype.getSavedSyncToken = degradable(function() {
-    return this.backend.getNextBatchToken();
-}, "getSavedSyncToken"),
+    _reallySave = degradable(function () {
+        this._syncTs = Date.now(); // set now to guard against multi-writes
 
-/**
- * Delete all data from this store.
- * @return {Promise} Resolves if the data was deleted from the database.
- */
-IndexedDBStore.prototype.deleteAllData = degradable(function() {
-    MemoryStore.prototype.deleteAllData.call(this);
-    return this.backend.clearDatabase().then(() => {
-        logger.log("Deleted indexeddb data.");
-    }, (err) => {
-        logger.error(`Failed to delete indexeddb data: ${err}`);
-        throw err;
+        // work out changed users (this doesn't handle deletions but you
+        // can't 'delete' users as they are just presence events).
+        const userTuples = [];
+        for (const u of this.getUsers()) {
+            if (this._userModifiedMap[u.userId] === u.getLastModifiedTime()) continue;
+            if (!u.events.presence) continue;
+
+            userTuples.push([u.userId, u.events.presence.event]);
+
+            // note that we've saved this version of the user
+            this._userModifiedMap[u.userId] = u.getLastModifiedTime();
+        }
+
+        return this.backend.syncToDatabase(userTuples);
     });
-});
 
-/**
- * Whether this store would like to save its data
- * Note that obviously whether the store wants to save or
- * not could change between calling this function and calling
- * save().
- *
- * @return {boolean} True if calling save() will actually save
- *     (at the time this function is called).
- */
-IndexedDBStore.prototype.wantsSave = function() {
-    const now = Date.now();
-    return now - this._syncTs > WRITE_DELAY_MS;
-};
+    setSyncData = degradable(function (syncData) {
+        return this.backend.setSyncData(syncData);
+    }, "setSyncData");
 
-/**
- * Possibly write data to the database.
- *
- * @param {bool} force True to force a save to happen
- * @return {Promise} Promise resolves after the write completes
- *     (or immediately if no write is performed)
- */
-IndexedDBStore.prototype.save = function(force) {
-    if (force || this.wantsSave()) {
-        return this._reallySave();
-    }
-    return Promise.resolve();
-};
+    /**
+     * Returns the out-of-band membership events for this room that
+     * were previously loaded.
+     * @param {string} roomId
+     * @returns {event[]} the events, potentially an empty array if OOB loading didn't yield any new members
+     * @returns {null} in case the members for this room haven't been stored yet
+     */
+    getOutOfBandMembers = degradable(function (roomId) {
+        return this.backend.getOutOfBandMembers(roomId);
+    }, "getOutOfBandMembers");
 
-IndexedDBStore.prototype._reallySave = degradable(function() {
-    this._syncTs = Date.now(); // set now to guard against multi-writes
+    /**
+     * Stores the out-of-band membership events for this room. Note that
+     * it still makes sense to store an empty array as the OOB status for the room is
+     * marked as fetched, and getOutOfBandMembers will return an empty array instead of null
+     * @param {string} roomId
+     * @param {event[]} membershipEvents the membership events to store
+     * @returns {Promise} when all members have been stored
+     */
+    setOutOfBandMembers = degradable(function (
+        roomId,
+        membershipEvents,
+    ) {
+        MemoryStore.prototype.setOutOfBandMembers.call(this, roomId, membershipEvents);
+        return this.backend.setOutOfBandMembers(roomId, membershipEvents);
+    }, "setOutOfBandMembers");
 
-    // work out changed users (this doesn't handle deletions but you
-    // can't 'delete' users as they are just presence events).
-    const userTuples = [];
-    for (const u of this.getUsers()) {
-        if (this._userModifiedMap[u.userId] === u.getLastModifiedTime()) continue;
-        if (!u.events.presence) continue;
+    clearOutOfBandMembers = degradable(function (roomId) {
+        MemoryStore.prototype.clearOutOfBandMembers.call(this);
+        return this.backend.clearOutOfBandMembers(roomId);
+    }, "clearOutOfBandMembers");
 
-        userTuples.push([u.userId, u.events.presence.event]);
+    getClientOptions = degradable(function () {
+        return this.backend.getClientOptions();
+    }, "getClientOptions");
 
-        // note that we've saved this version of the user
-        this._userModifiedMap[u.userId] = u.getLastModifiedTime();
-    }
+    storeClientOptions = degradable(function (options) {
+        MemoryStore.prototype.storeClientOptions.call(this, options);
+        return this.backend.storeClientOptions(options);
+    }, "storeClientOptions");
 
-    return this.backend.syncToDatabase(userTuples);
-});
-
-IndexedDBStore.prototype.setSyncData = degradable(function(syncData) {
-    return this.backend.setSyncData(syncData);
-}, "setSyncData");
-
-/**
- * Returns the out-of-band membership events for this room that
- * were previously loaded.
- * @param {string} roomId
- * @returns {event[]} the events, potentially an empty array if OOB loading didn't yield any new members
- * @returns {null} in case the members for this room haven't been stored yet
- */
-IndexedDBStore.prototype.getOutOfBandMembers = degradable(function(roomId) {
-    return this.backend.getOutOfBandMembers(roomId);
-}, "getOutOfBandMembers");
-
-/**
- * Stores the out-of-band membership events for this room. Note that
- * it still makes sense to store an empty array as the OOB status for the room is
- * marked as fetched, and getOutOfBandMembers will return an empty array instead of null
- * @param {string} roomId
- * @param {event[]} membershipEvents the membership events to store
- * @returns {Promise} when all members have been stored
- */
-IndexedDBStore.prototype.setOutOfBandMembers = degradable(function(
-    roomId,
-    membershipEvents,
-) {
-    MemoryStore.prototype.setOutOfBandMembers.call(this, roomId, membershipEvents);
-    return this.backend.setOutOfBandMembers(roomId, membershipEvents);
-}, "setOutOfBandMembers");
-
-IndexedDBStore.prototype.clearOutOfBandMembers = degradable(function(roomId) {
-    MemoryStore.prototype.clearOutOfBandMembers.call(this);
-    return this.backend.clearOutOfBandMembers(roomId);
-}, "clearOutOfBandMembers");
-
-IndexedDBStore.prototype.getClientOptions = degradable(function() {
-    return this.backend.getClientOptions();
-}, "getClientOptions");
-
-IndexedDBStore.prototype.storeClientOptions = degradable(function(options) {
-    MemoryStore.prototype.storeClientOptions.call(this, options);
-    return this.backend.storeClientOptions(options);
-}, "storeClientOptions");
-
+}
+utils.extend(IndexedDBStore.prototype, EventEmitter.prototype);
 /**
  * All member functions of `IndexedDBStore` that access the backend use this wrapper to
  * watch for failures after initial store startup, including `QuotaExceededError` as
@@ -283,11 +284,11 @@ IndexedDBStore.prototype.storeClientOptions = degradable(function(options) {
  * in place so that the current operation and all future ones are in-memory only.
  *
  * @param {Function} func The degradable work to do.
- * @param {String} fallback The method name for fallback.
+ * @param {String} [fallback] The method name for fallback.
  * @returns {Function} A wrapped member function.
  */
 function degradable(func, fallback) {
-    return async function(...args) {
+    return async function (...args) {
         try {
             return await func.call(this, ...args);
         } catch (e) {
