a95f6da71a5dfc825fca7ae2bbacf7ea41e827c8
Travis Ralston <travpc@gmail.com>
2020-02-24 14:46:53 -0700

    Cleanup errors.js

--- src/errors.js
+++ src/errors.js
@@ -1,4 +1,6 @@
 export class InvalidStoreError extends Error {
+    static TOGGLED_LAZY_LOADING = "TOGGLED_LAZY_LOADING";
+
     constructor(reason, value) {
         super(reason);
         const message = `Store is invalid because ${reason}, ` +
@@ -9,12 +11,13 @@ export class InvalidStoreError extends Error {
         instance.value = value;
         return instance;
     }
-    static TOGGLED_LAZY_LOADING = "TOGGLED_LAZY_LOADING";
 }
 
 export class InvalidCryptoStoreError extends Error {
+    static TOO_NEW = "TOO_NEW";
+
     constructor(reason) {
-        super(reason)
+        super(reason);
         const message = `Crypto store is invalid because ${reason}, ` +
             `please stop the client, delete all data and start the client again`;
         const instance = Reflect.construct(Error, [message]);
@@ -23,5 +26,4 @@ export class InvalidCryptoStoreError extends Error {
         instance.name = 'InvalidCryptoStoreError';
         return instance;
     }
-    static TOO_NEW = "TOO_NEW";
 }
