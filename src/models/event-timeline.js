03ed11bf2d9a2fc98aef19fc8e94fc316353b0d1
Travis Ralston <travpc@gmail.com>
2020-02-28 17:06:39 -0700

    Make setEventMetadata static again

--- src/models/event-timeline.js
+++ src/models/event-timeline.js
@@ -347,7 +347,7 @@ export class EventTimeline {
      * @param {RoomState} stateContext  the room state to be queried
      * @param {bool} toStartOfTimeline  if true the event's forwardLooking flag is set false
      */
-    setEventMetadata(event, stateContext, toStartOfTimeline) {
+    static setEventMetadata(event, stateContext, toStartOfTimeline) {
         // set sender and target properties
         event.sender = stateContext.getSentinelMember(
             event.getSender(),
