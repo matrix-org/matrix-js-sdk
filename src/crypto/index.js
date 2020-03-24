cea3582ed196e97314acb6bb5ab815a363abceb0
J. Ryan Stinnett <jryans@gmail.com>
2020-03-24 18:28:31 +0000

    Always attempt caching via bootstrap

--- src/crypto/index.js
+++ src/crypto/index.js
@@ -632,13 +632,14 @@ Crypto.prototype.bootstrapSecretStorage = async function({
                     this._secretStorage,
                 );
             }
-            // Call `getCrossSigningKey` for side effect of caching if enabled
-            // via app level callbacks.
-            if (this._crossSigningInfo._cacheCallbacks) {
-                for (const type of ["self_signing", "user_signing"]) {
-                    logger.log(`Cache ${type} cross-signing private key locally`);
-                    await this._crossSigningInfo.getCrossSigningKey(type);
-                }
+        }
+
+        // Call `getCrossSigningKey` for side effect of caching private keys for
+        // future gossiping to other devices if enabled via app level callbacks.
+        if (this._crossSigningInfo._cacheCallbacks) {
+            for (const type of ["self_signing", "user_signing"]) {
+                logger.log(`Cache ${type} cross-signing private key locally`);
+                await this._crossSigningInfo.getCrossSigningKey(type);
             }
         }
 
