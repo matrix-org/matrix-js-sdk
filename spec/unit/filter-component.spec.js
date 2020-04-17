6cf8a76c29c4043141d9688c4424cf74a5c1d6cd
Nicolas Chamo <nicolas@chamo.com.ar>
2020-04-17 19:50:15 -0300

    linting

--- spec/unit/filter-component.spec.js
+++ spec/unit/filter-component.spec.js
@@ -1,35 +1,33 @@
-import {FilterComponent} from "../../src/filter-component"
-import {mkEvent} from '../test-utils'
+import {FilterComponent} from "../../src/filter-component";
+import {mkEvent} from '../test-utils';
 
 describe("Filter Component", function() {
 
     describe("types", function() {
         it("should filter out events with other types", function() {
-            const filter = new FilterComponent({ types: [ 'm.room.message' ] })
-
+            const filter = new FilterComponent({ types: ['m.room.message'] });
             const event = mkEvent({
                 type: 'm.room.member',
-                content: {  },
+                content: { },
                 room: 'roomId',
                 event: true,
-            })
+            });
 
-            const checkResult = filter.check(event)
+            const checkResult = filter.check(event);
 
             expect(checkResult).toBe(false);
         });
 
         it("should validate events with the same type", function() {
-            const filter = new FilterComponent({ types: [ 'm.room.message' ] })
-
+            const filter = new FilterComponent({ types: ['m.room.message'] });
             const event = mkEvent({
                 type: 'm.room.message',
-                content: {  },
+                content: { },
                 room: 'roomId',
                 event: true,
-            })
+            });
 
-            const checkResult = filter.check(event)
+            const checkResult = filter.check(event);
 
             expect(checkResult).toBe(true);
         });
