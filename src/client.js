0c47412c757ec0f51e075af69e44e7d6bdd8446e
Germain Souquet <germain@souquet.com>
2021-05-19 17:15:44 +0100

    move startEventCallHandler to MatrixClient prototype

--- src/client.js
+++ src/client.js
@@ -356,13 +356,7 @@ export function MatrixClient(opts) {
         // Start listening for calls after the initial sync is done
         // We do not need to backfill the call event buffer
         // with encrypted events that might never get decrypted
-        function startCallEventHandler() {
-            if (this.isInitialSyncComplete()) {
-                this._callEventHandler.start();
-                this.off("sync", startCallEventHandler);
-            }
-        }
-        this.on("sync", startCallEventHandler);
+        this.on("sync", this._startCallEventHandler);
     } else {
         this._callEventHandler = null;
     }
@@ -4995,6 +4989,13 @@ MatrixClient.prototype.getOpenIdToken = function() {
 
 // VoIP operations
 // ===============
+
+MatrixClient.prototype._startCallEventHandler = function() {
+    if (this.isInitialSyncComplete()) {
+        this._callEventHandler.start();
+        this.off("sync", this._startCallEventHandler);
+    }
+};
 
 /**
  * @param {module:client.callback} callback Optional.
