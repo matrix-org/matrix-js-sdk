0a0489750c488bbdaad0552f8fac7920304adacb
Šimon Brandner <simon.bra.ag@gmail.com>
2021-05-28 17:37:55 +0200

    Add missing space
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- spec/unit/webrtc/call.spec.ts
+++ spec/unit/webrtc/call.spec.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { TestClient} from '../../TestClient';
+import { TestClient } from '../../TestClient';
 import { MatrixCall, CallErrorCode, CallEvent } from '../../../src/webrtc/call';
 import { SDPStreamMetadataKey, SDPStreamMetadataPurpose } from '../../../src/webrtc/callEventTypes';
 
