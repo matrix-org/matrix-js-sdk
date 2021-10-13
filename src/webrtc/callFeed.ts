411b5f111c26ae2d7dd4738036bb7abf1bd7899d
Robert Long <robert@robertlong.me>
2021-10-13 13:42:40 -0700

    Fix talking indicator

--- src/webrtc/callFeed.ts
+++ src/webrtc/callFeed.ts
@@ -78,6 +78,10 @@ export class CallFeed extends EventEmitter {
     }
 
     private updateStream(oldStream: MediaStream, newStream: MediaStream): void {
+        if (newStream === oldStream) {
+            return;
+        }
+
         if (oldStream) {
             oldStream.removeEventListener("addtrack", this.onAddTrack);
             this.measureVolumeActivity(false);
