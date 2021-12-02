e1a598cc2c2cc594c0b4ba4f7cb4944e13a7212e
Michael Telatynski <7t3chguy@gmail.com>
2021-12-02 12:26:21 +0000

    Add method to fetch /account/whoami

--- src/client.ts
+++ src/client.ts
@@ -8892,6 +8892,13 @@ export class MatrixClient extends EventEmitter {
                 room.addThreadedEvent(event);
             });
     }
+
+    /**
+     * Fetches the user_id of the configured access token.
+     */
+    public async whoami(): Promise<{ user_id: string }> { // eslint-disable-line camelcase
+        return this.http.authedRequest(undefined, "GET", "/account/whoami");
+    }
 }
 
 /**
