a13ed24375071388bf63eedc490ad697abf0e84b
Hugh Nimmo-Smith <hughns@element.io>
2021-12-22 11:25:50 +0000

    Fix more function typings relating to key backup

--- src/client.ts
+++ src/client.ts
@@ -2658,7 +2658,11 @@ export class MatrixClient extends EventEmitter {
         );
     }
 
-    private makeKeyBackupPath(roomId: string, sessionId: string, version: string): IKeyBackupPath {
+    private makeKeyBackupPath(
+        roomId: string | undefined,
+        sessionId: string | undefined,
+        version: string,
+    ): IKeyBackupPath {
         let path;
         if (sessionId !== undefined) {
             path = utils.encodeUri("/room_keys/keys/$roomId/$sessionId", {
@@ -2685,7 +2689,12 @@ export class MatrixClient extends EventEmitter {
      * @return {Promise} a promise that will resolve when the keys
      * are uploaded
      */
-    public sendKeyBackup(roomId: string, sessionId: string, version: string, data: IKeyBackup): Promise<void> {
+    public sendKeyBackup(
+        roomId: string | undefined,
+        sessionId: string | undefined,
+        version: string,
+        data: IKeyBackup,
+    ): Promise<void> {
         if (!this.crypto) {
             throw new Error("End-to-end encryption disabled");
         }
@@ -2773,8 +2782,8 @@ export class MatrixClient extends EventEmitter {
      */
     public async restoreKeyBackupWithPassword(
         password: string,
-        targetRoomId: string,
-        targetSessionId: string,
+        targetRoomId: string | undefined,
+        targetSessionId: string | undefined,
         backupInfo: IKeyBackupInfo,
         opts: IKeyBackupRestoreOpts,
     ): Promise<IKeyBackupRestoreResult> {
@@ -2835,8 +2844,8 @@ export class MatrixClient extends EventEmitter {
      */
     public restoreKeyBackupWithRecoveryKey(
         recoveryKey: string,
-        targetRoomId: string,
-        targetSessionId: string,
+        targetRoomId: string | undefined,
+        targetSessionId: string | undefined,
         backupInfo: IKeyBackupInfo,
         opts: IKeyBackupRestoreOpts,
     ): Promise<IKeyBackupRestoreResult> {
@@ -2845,8 +2854,8 @@ export class MatrixClient extends EventEmitter {
     }
 
     public async restoreKeyBackupWithCache(
-        targetRoomId: string,
-        targetSessionId: string,
+        targetRoomId: string | undefined,
+        targetSessionId: string | undefined,
         backupInfo: IKeyBackupInfo,
         opts?: IKeyBackupRestoreOpts,
     ): Promise<IKeyBackupRestoreResult> {
@@ -2859,8 +2868,8 @@ export class MatrixClient extends EventEmitter {
 
     private async restoreKeyBackup(
         privKey: ArrayLike<number>,
-        targetRoomId: string,
-        targetSessionId: string,
+        targetRoomId: string | undefined,
+        targetSessionId: string | undefined,
         backupInfo: IKeyBackupInfo,
         opts?: IKeyBackupRestoreOpts,
     ): Promise<IKeyBackupRestoreResult> {
@@ -2953,7 +2962,11 @@ export class MatrixClient extends EventEmitter {
         return { total: totalKeyCount, imported: keys.length };
     }
 
-    public deleteKeysFromBackup(roomId: string, sessionId: string, version: string): Promise<void> {
+    public deleteKeysFromBackup(
+        roomId: string | undefined,
+        sessionId: string | undefined,
+        version: string,
+    ): Promise<void> {
         if (!this.crypto) {
             throw new Error("End-to-end encryption disabled");
         }
