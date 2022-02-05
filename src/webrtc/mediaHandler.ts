0335bc564913230c748ff44e51639280e755ec3a
Šimon Brandner <simon.bra.ag@gmail.com>
2022-02-05 11:32:36 +0100

    Add copyright for 2022
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/webrtc/mediaHandler.ts
+++ src/webrtc/mediaHandler.ts
@@ -2,7 +2,7 @@
 Copyright 2015, 2016 OpenMarket Ltd
 Copyright 2017 New Vector Ltd
 Copyright 2019, 2020 The Matrix.org Foundation C.I.C.
-Copyright 2021 Šimon Brandner <simon.bra.ag@gmail.com>
+Copyright 2021 - 2022 Šimon Brandner <simon.bra.ag@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
