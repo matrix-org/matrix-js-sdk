25c115739cb767d55cae81eca2e7dbf3ce0d6883
Hugh Nimmo-Smith <hughns@users.noreply.github.com>
2022-02-11 11:16:10 +0000

    Fix error in uploadContent() when file is empty under Node.js (#2155)
    * Fix error in uploadContent() when file is empty under Node.js
    
    * Make type safe check work
    
    * Make comment actually make sense

--- src/http-api.ts
+++ src/http-api.ts
@@ -437,10 +437,20 @@ export class MatrixHttpApi {
                 queryParams.filename = fileName;
             }
 
+            const headers: Record<string, string> = { "Content-Type": contentType };
+
+            // authedRequest uses `request` which is no longer maintained.
+            // `request` has a bug where if the body is zero bytes then you get an error: `Argument error, options.body`.
+            // See https://github.com/request/request/issues/920
+            // if body looks like a byte array and empty then set the Content-Length explicitly as a workaround:
+            if ((body as unknown as ArrayLike<number>).length === 0) {
+                headers["Content-Length"] = "0";
+            }
+
             promise = this.authedRequest(
                 opts.callback, Method.Post, "/upload", queryParams, body, {
                     prefix: "/_matrix/media/r0",
-                    headers: { "Content-Type": contentType },
+                    headers,
                     json: false,
                     bodyParser,
                 },
