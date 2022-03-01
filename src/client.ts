05bf1eba7d638ba36da81d85729822bd24c76114
Michael Telatynski <7t3chguy@gmail.com>
2022-03-01 18:06:09 +0000

    Revert "Improve typing around event emitter handlers (#7816)"
    This reverts commit 7fa01ffb

--- src/client.ts
+++ src/client.ts
@@ -8712,17 +8712,10 @@ export class MatrixClient extends TypedEventEmitter<EmittedEvents, ClientEventHa
 
         const queryParams: Record<string, string | string[]> = {
             suggested_only: String(suggestedOnly),
+            max_depth: maxDepth?.toString(),
+            from: fromToken,
+            limit: limit?.toString(),
         };
-
-        if (limit !== undefined) {
-            queryParams["limit"] = limit.toString();
-        }
-        if (maxDepth !== undefined) {
-            queryParams["max_depth"] = maxDepth.toString();
-        }
-        if (fromToken !== undefined) {
-            queryParams["from"] = fromToken;
-        }
 
         return this.http.authedRequest<IRoomHierarchy>(undefined, Method.Get, path, queryParams, undefined, {
             prefix: PREFIX_V1,
