c503a415552a64425ed8433ec24badd48510c518
Michael Telatynski <7t3chguy@gmail.com>
2022-05-01 11:25:52 +0100

    Make type more flexible

--- src/utils.ts
+++ src/utils.ts
@@ -464,7 +464,7 @@ export function defer<T = void>(): IDeferred<T> {
 }
 
 export async function promiseMapSeries<T>(
-    promises: Promise<T>[],
+    promises: Array<T | Promise<T>>,
     fn: (t: T) => void,
 ): Promise<void> {
     for (const o of promises) {
