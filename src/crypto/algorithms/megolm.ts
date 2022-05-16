81494e6c724ae1e1d383060c591db0bf8da729ab
Faye Duxovni <fayed@matrix.org>
2022-05-16 12:50:02 -0400

    don't deadlock waiting on key backup query

--- src/crypto/algorithms/megolm.ts
+++ src/crypto/algorithms/megolm.ts
@@ -1299,11 +1299,7 @@ class MegolmDecryption extends DecryptionAlgorithm {
         if (res === null) {
             // We've got a message for a session we don't have.
 
-            try {
-                await this.baseApis.restoreKeyBackupWithBackupManager(event.getRoomId(), content.session_id, this.crypto.backupManager.backupInfo, {});
-            } catch (e) {
-                logger.warn("Unable to retrieve key from backup");
-            }
+            this.crypto.backupManager.queryKeyBackupRateLimited(event.getRoomId(), content.session_id);
             //
             // (XXX: We might actually have received this key since we started
             // decrypting, in which case we'll have scheduled a retry, and this
