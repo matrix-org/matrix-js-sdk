e4937e1f945879088513d8296b55ffe9e8d29f30
Šimon Brandner <simon.bra.ag@gmail.com>
2022-06-18 11:49:00 +0200

    Use plural
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/@types/requests.ts
+++ src/@types/requests.ts
@@ -113,7 +113,7 @@ export interface IRoomDirectoryOptions {
     since?: string;
     filter?: {
         generic_search_term?: string;
-        room_type?: Set<RoomType | null>;
+        room_types?: Set<RoomType | null>;
     };
     include_all_networks?: boolean;
     third_party_instance_id?: string;
