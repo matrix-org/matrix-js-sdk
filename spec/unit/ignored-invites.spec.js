608e87bb7eeb553391c7531fb807733210004b99
David Teller <d.o.teller+github@gmail.com>
2022-07-06 13:38:46 +0200

    WIP: Adding some tests

---
+++ spec/unit/ignored-invites.spec.js
@@ -0,0 +1,48 @@
+import { MatrixEvent } from '../../src';
+import { TestClient } from '../TestClient';
+
+describe('Store ignored invites', function() {
+    let client;
+
+    beforeEach(function() {
+        client = new TestClient("foobar", "device");
+
+        // Mockup account data storage.
+        let dataStore = new Map();
+        client.client.setAccountData = function (eventType, content) {
+            dataStore.set(eventType, content);
+            return Promise.resolve();
+        }
+        client.client.getAccountData = function (eventType) {
+            let data = dataStore.get(eventType);
+            return new MatrixEvent({
+                content: data
+            });
+        };   
+    });
+
+    afterEach(function() {
+        client.stop();
+    });
+
+    it('Stores ignored invites', async function() {
+        // Initially, the invite list should be empty but not `null`.
+        expect(await client.client.getIgnoredInvites()).toEqual({});
+
+        // Insert something, we should be able to recover it.
+        const SAMPLE = {
+            ignored_rooms: [
+                {
+                    room_id: "12345",
+                    ts: Date.now(),
+                }
+            ]
+        };
+        let promise = client.client.setIgnoredInvites(SAMPLE);
+        client.httpBackend.flush();
+        await promise;
+
+        // Check that it was (mock)stored on the client.
+        expect(await client.client.getIgnoredInvites()).toEqual(SAMPLE);
+    });
+});
