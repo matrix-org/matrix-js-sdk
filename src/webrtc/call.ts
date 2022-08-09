bf8bfbaecae9a90de3e27516160004d716171de9
Šimon Brandner <simon.bra.ag@gmail.com>
2022-08-09 16:32:54 +0200

    Types?
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/webrtc/call.ts
+++ src/webrtc/call.ts
@@ -628,7 +628,7 @@ export class MatrixCall extends TypedEventEmitter<CallEvent, CallEventHandlerMap
                 audio_muted: feed.isAudioMuted(),
                 video_muted: feed.isVideoMuted(),
                 tracks: transceivers.map((transceiver) => {
-                    const media = sdp?.media?.find((m) => m.mid === transceiver.mid);
+                    const media = sdp?.media?.find((m) => m.mid == transceiver.mid);
                     const splitMsid = media?.msid?.split(" ");
                     logger.warn("Media with matching mid", media);
                     if (splitMsid?.[1]) {
