2334aa0db88bea7cf9ff56f05aec23779cd39df2
Hugh Nimmo-Smith <hughns@users.noreply.github.com>
2022-09-01 17:46:40 +0100

    Update src/client.ts
    Co-authored-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/client.ts
+++ src/client.ts
@@ -7160,8 +7160,10 @@ export class MatrixClient extends TypedEventEmitter<EmittedEvents, ClientEventHa
             url += "/" + idpId;
         }
 
-        const params = { redirectUrl };
-        params[SSO_ACTION_PARAM.unstable] = action;
+        const params = { 
+            redirectUrl,
+            [SSO_ACTION_PARAM.unstable]: action,
+        };
 
         return this.http.getUrl(url, params, PREFIX_R0);
     }
