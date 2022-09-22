3032253c355240f322340c3b0b39f1abc569364e
Hugh Nimmo-Smith <hughns@matrix.org>
2022-09-22 13:31:18 +0100

    Add tests

--- spec/integ/matrix-client-methods.spec.js
+++ spec/integ/matrix-client-methods.spec.js
@@ -1074,6 +1074,30 @@ describe("MatrixClient", function() {
             expect(await prom).toStrictEqual(response);
         });
     });
+
+    describe("requestLoginToken", () => {
+        it("should hit the expected API endpoin with UIA", async () => {
+            const response = {};
+            const uiaData = { foo: "baa" };
+            const prom = client.requestLoginToken(uiaData);
+            httpBackend
+                .when("POST", "/unstable/org.matrix.msc3882/login/token", { auth: uiaData })
+                .respond(200, response);
+            await httpBackend.flush();
+            expect(await prom).toStrictEqual(response);
+        });
+
+        it("should hit the expected API endpoin without UIA", async () => {
+            const response = {};
+            const prom = client.requestLoginToken();
+            httpBackend
+                .when("POST", "/unstable/org.matrix.msc3882/login/token", {})
+                .respond(200, response);
+            await httpBackend.flush();
+            expect(await prom).toStrictEqual(response);
+        });
+    });
+
 });
 
 function withThreadId(event, newThreadId) {
