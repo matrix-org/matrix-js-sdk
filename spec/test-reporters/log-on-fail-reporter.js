d76c6c264814be79b94849c5c6cf8959dd8261e1
Michael Telatynski <7t3chguy@gmail.com>
2022-09-23 09:19:02 +0100

    Delint

--- spec/test-reporters/log-on-fail-reporter.js
+++ spec/test-reporters/log-on-fail-reporter.js
@@ -1,14 +1,15 @@
+// Based on https://github.com/facebook/jest/issues/4156#issuecomment-757376195
 const { DefaultReporter } = require('@jest/reporters');
 
 class Reporter extends DefaultReporter {
-    printTestFileHeader(_testPath, config, result) {
+    printTestFileHeader(testPath, config, result) {
         const console = result.console;
 
         if (result.numFailingTests === 0 && !result.testExecError) {
             result.console = null;
         }
 
-        super.printTestFileHeader(...arguments);
+        super.printTestFileHeader(testPath, config, result);
 
         result.console = console;
     }
