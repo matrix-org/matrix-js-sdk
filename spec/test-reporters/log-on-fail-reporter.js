1cc94bedae003574a9b62e24a225ce4deedb42f3
Michael Telatynski <7t3chguy@gmail.com>
2022-09-23 09:09:08 +0100

    Jest silence console on successful test suites

---
+++ spec/test-reporters/log-on-fail-reporter.js
@@ -0,0 +1,17 @@
+const { DefaultReporter } = require('@jest/reporters');
+
+class Reporter extends DefaultReporter {
+    printTestFileHeader(_testPath, config, result) {
+        const console = result.console;
+
+        if (result.numFailingTests === 0 && !result.testExecError) {
+            result.console = null;
+        }
+
+        super.printTestFileHeader(...arguments);
+
+        result.console = console;
+    }
+}
+
+module.exports = Reporter;
