15d269d079f3add9b024bedb29174f474d4a0a55
Germain Souquet <germains@element.io>
2022-10-03 16:57:59 +0100

    Fix unread_thread_notifications filter definition

--- src/filter.ts
+++ src/filter.ts
@@ -228,7 +228,7 @@ export class Filter {
      * @param {boolean} enabled
      */
     public setUnreadThreadNotifications(enabled: boolean): void {
-        setProp(this.definition, `room.state.${UNREAD_THREAD_NOTIFICATIONS.name}`, !!enabled);
+        setProp(this.definition, `room.timeline.${UNREAD_THREAD_NOTIFICATIONS.name}`, !!enabled);
     }
 
     setLazyLoadMembers(enabled: boolean): void {
