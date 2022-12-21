83dcf01c48986fce199159efcbfa53e6961f2317
Travis Ralston <travisr@matrix.org>
2022-12-21 14:11:03 -0700

    Missed one.

--- src/autodiscovery.ts
+++ src/autodiscovery.ts
@@ -216,7 +216,7 @@ export class AutoDiscovery {
             // URL.
             const isResponse = await this.fetchWellKnownObject(`${isUrl}/_matrix/identity/v2`);
             if (!isResponse?.raw || isResponse.action !== AutoDiscoveryAction.SUCCESS) {
-                logger.error("Invalid /api/v1 response");
+                logger.error("Invalid /v2 response");
                 failingClientConfig["m.identity_server"].error = AutoDiscovery.ERROR_INVALID_IDENTITY_SERVER;
 
                 // Supply the base_url to the caller because they may be ignoring
