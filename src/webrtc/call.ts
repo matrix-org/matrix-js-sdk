2804fed0be553b3a385fc2ff0df0a95adfb1a327
Šimon Brandner <simon.bra.ag@gmail.com>
2023-01-25 16:37:52 +0100

    Remove obsolete comment
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/webrtc/call.ts
+++ src/webrtc/call.ts
@@ -847,8 +847,6 @@ export class MatrixCall extends TypedEventEmitter<CallEvent, CallEventHandlerMap
                     // Matrix event.
                     const transceiver = this.transceivers.get(tKey)!;
 
-                    // this is what would allow us to use addTransceiver(), but it's not available
-                    // on Firefox yet. We call it anyway if we have it.
                     if (transceiver.sender.setStreams) transceiver.sender.setStreams(callFeed.stream!);
 
                     transceiver.sender.replaceTrack(track);
