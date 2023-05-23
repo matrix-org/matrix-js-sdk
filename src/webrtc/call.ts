a244c53c1419f096c89b8c6afa78ddb2f62d5b02
Enrico Schwendig <enricos@element.io>
2023-05-23 11:09:15 +0200

    Force a TURN server connection

--- src/webrtc/call.ts
+++ src/webrtc/call.ts
@@ -2906,11 +2906,10 @@ export class MatrixCall extends TypedEventEmitter<CallEvent, CallEventHandlerMap
                 logger.debug(
                     `Call ${this.callId} addIceCandidates() got remote ICE candidate (sdpMid=${candidate.sdpMid}, candidate=${candidate.candidate})`,
                 );
-
-                // Force to use TURN
-                if (candidate.type !== "relay") {
-                    return;
-                }
+            }
+            // Force to use TURN
+            if (!!candidate && candidate.type !== "relay") {
+                return;
             }
 
             try {
