33e8f5cee24847ba0fd884b15664fe176e9f1976
Michael Weimann <michaelw@matrix.org>
2023-06-01 14:27:30 +0200

    Fix typo

--- spec/unit/matrix-client.spec.ts
+++ spec/unit/matrix-client.spec.ts
@@ -340,7 +340,7 @@ describe("MatrixClient", function () {
         store.storeClientOptions = jest.fn().mockReturnValue(Promise.resolve(null));
         store.isNewlyCreated = jest.fn().mockReturnValue(Promise.resolve(true));
 
-        // set unstableFeatures to a defined state before reach test
+        // set unstableFeatures to a defined state before each test
         unstableFeatures = {
             "org.matrix.msc3440.stable": true,
         };
