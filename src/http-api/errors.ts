50e572b4377ac7ecc2ca41149126734565f27ecf
Michael Telatynski <7t3chguy@gmail.com>
2023-07-24 13:00:24 +0100

    Fix registration check your emails stage regression

--- src/http-api/errors.ts
+++ src/http-api/errors.ts
@@ -17,7 +17,7 @@ limitations under the License.
 import { IUsageLimit } from "../@types/partials";
 import { MatrixEvent } from "../models/event";
 
-interface IErrorJson extends Partial<IUsageLimit> {
+export interface MatrixErrorJson extends Partial<IUsageLimit> {
     [key: string]: any; // extensible
     errcode?: string;
     error?: string;
@@ -39,7 +39,7 @@ export class MatrixError extends HTTPError {
     // The Matrix 'errcode' value, e.g. "M_FORBIDDEN".
     public readonly errcode?: string;
     // The raw Matrix error JSON used to construct this object.
-    public data: IErrorJson;
+    public data: MatrixErrorJson;
 
     /**
      * Construct a Matrix error. This is a JavaScript Error with additional
@@ -48,7 +48,7 @@ export class MatrixError extends HTTPError {
      * @param httpStatus - The numeric HTTP status code given
      */
     public constructor(
-        errorJson: IErrorJson = {},
+        errorJson: MatrixErrorJson = {},
         public readonly httpStatus?: number,
         public url?: string,
         public event?: MatrixEvent,
--- src/interactive-auth.ts
+++ src/interactive-auth.ts
@@ -19,7 +19,7 @@ limitations under the License.
 import { logger } from "./logger";
 import { MatrixClient } from "./client";
 import { defer, IDeferred } from "./utils";
-import { MatrixError } from "./http-api";
+import { MatrixError, MatrixErrorJson } from "./http-api";
 import { UIAResponse } from "./@types/uia";
 import { UserIdentifier } from "./@types/auth";
 
@@ -264,8 +264,8 @@ export class InteractiveAuth<T> {
     private readonly requestEmailTokenCallback: IOpts<T>["requestEmailToken"];
     private readonly supportedStages?: Set<string>;
 
-    // The current latest data received from the server during the user interactive auth flow.
-    private data: IAuthData;
+    // The current latest data or error received from the server during the user interactive auth flow.
+    private data: IAuthData & MatrixErrorJson;
     private emailSid?: string;
     private requestingEmailToken = false;
     private attemptAuthDeferred: IDeferred<T> | null = null;
@@ -549,7 +549,7 @@ export class InteractiveAuth<T> {
                 matrixError.data.session = (this.data as IAuthData).session;
             }
             if (matrixError) {
-                this.data = matrixError.data as IAuthData;
+                this.data = matrixError.data;
             }
             try {
                 this.startNextAuthStage();
@@ -598,6 +598,14 @@ export class InteractiveAuth<T> {
         if (nextStage === AuthType.Dummy) {
             this.submitAuthDict({
                 type: "m.login.dummy",
+            });
+            return;
+        }
+
+        if (this.data?.errcode || this.data?.error) {
+            this.stateUpdatedCallback(nextStage, {
+                errcode: this.data?.errcode || "",
+                error: this.data?.error || "",
             });
             return;
         }
