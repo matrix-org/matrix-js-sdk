c4cf319c0b1c671b530d922e1216f51df24a9510
Šimon Brandner <simon.bra.ag@gmail.com>
2023-08-31 14:00:40 +0200

    Revert making `joinRoomSession()` async
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/matrixrtc/MatrixRTCSession.ts
+++ src/matrixrtc/MatrixRTCSession.ts
@@ -172,7 +172,7 @@ export class MatrixRTCSession extends TypedEventEmitter<MatrixRTCSessionEvent, M
      * This will not subscribe to updates: remember to call subscribe() separately if
      * desired.
      */
-    public async joinRoomSession(activeFoci: Focus[], encryptMedia?: boolean): Promise<void> {
+    public joinRoomSession(activeFoci: Focus[], encryptMedia?: boolean): void {
         if (this.isJoined()) {
             logger.info(`Already joined to session in room ${this.room.roomId}: ignoring join call`);
             return;
@@ -183,7 +183,7 @@ export class MatrixRTCSession extends TypedEventEmitter<MatrixRTCSessionEvent, M
         this.relativeExpiry = MEMBERSHIP_EXPIRY_TIME;
         this.encryptMedia = encryptMedia ?? false;
         this.emit(MatrixRTCSessionEvent.JoinStateChanged, true);
-        await this.updateCallMembershipEvent();
+        this.updateCallMembershipEvent();
     }
 
     /**
