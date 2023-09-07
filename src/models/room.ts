dd6908b97a8ead87e08a5710fb1a6ed8b8603907
Andy Balaam <andy.balaam@matrix.org>
2023-09-07 16:15:17 +0100

    Fix misleading "local echo" comment with "remote echo"
    Co-authored-by: Richard van der Hoff <1389908+richvdh@users.noreply.github.com>

--- src/models/room.ts
+++ src/models/room.ts
@@ -2948,7 +2948,7 @@ export class Room extends ReadReceipt<RoomEmittedEvents, RoomEventHandlerMap> {
                         // If the read receipt sent for the logged in user matches
                         // the last event of the live timeline, then we know for a fact
                         // that the user has read that message, so we can mark the room
-                        // as read and not wait for the local echo from synapse.
+                        // as read and not wait for the remote echo from synapse.
                         //
                         // This needs to be done after the initial sync as we do not want this
                         // logic to run whilst the room is being initialised
