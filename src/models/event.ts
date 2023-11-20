fff6e3c3045a82cca5e4540a8c89cb899c18d180
Timo <16718859+toger5@users.noreply.github.com>
2023-11-20 11:54:30 +0100

    Update src/models/event.ts
    Co-authored-by: Richard van der Hoff <1389908+richvdh@users.noreply.github.com>

--- src/models/event.ts
+++ src/models/event.ts
@@ -410,7 +410,7 @@ export class MatrixEvent extends TypedEventEmitter<MatrixEventEmittedEvents, Mat
         // The fallback in these cases will be to use the origin_server_ts.
         // For EDUs, the origin_server_ts also is not defined so we use Date.now().
         const age = this.getAge();
-        this.localTimestamp = age ? Date.now() - age : this.getTs() ?? Date.now();
+        this.localTimestamp = age !== undefined ? Date.now() - age : this.getTs() ?? Date.now();
         this.reEmitter = new TypedReEmitter(this);
     }
 
