54b71bb729090a7534a54bd6dbfdc3e3d738d66a
Timo K <toger5@hotmail.de>
2023-11-20 16:26:46 +0100

    Replace localTimestamp biding with binding the whole roomState
    Signed-off-by: Timo K <toger5@hotmail.de>

--- spec/unit/matrixrtc/mocks.ts
+++ spec/unit/matrixrtc/mocks.ts
@@ -20,49 +20,27 @@ import { randomString } from "../../../src/randomstring";
 
 export function makeMockRoom(memberships: CallMembershipData[], localAge: number | null = null): Room {
     const roomId = randomString(8);
-    // If makeMockRoom is used instead of one of the other functions in this file,
-    // we store the localTimestamp so that subsequent calls calculate the expiration based on the localTimestamp
-    // using Date.now(). So, jest.advanceTimersByTime() can be used to simulate the passage of time.
-
-    // The actual time of the underlying mocked events will be: localTimestamp ?? localAge ?? 0
-    const localTimestamp = Date.now() - (localAge ?? 10);
+    // Caching roomState here so it does not get recreated when calling `getLiveTimeline.getState()`
+    const roomState = makeMockRoomState(memberships, roomId, localAge);
     return {
         roomId: roomId,
         getLiveTimeline: jest.fn().mockReturnValue({
-            // When setting up `makeMockRoomState` we always want to use the localTimestamp instead of localAge.
-            getState: jest.fn().mockReturnValue(makeMockRoomState(memberships, roomId, null, localTimestamp)),
+            getState: jest.fn().mockReturnValue(roomState),
         }),
     } as unknown as Room;
 }
 
-export function makeMockRoomState(
-    memberships: CallMembershipData[],
-    roomId: string,
-    /**`localAge` is ignored if localTimestamp is present.*/
-    localAge: number | null = null,
-    localTimestamp: number | null = null,
-) {
+export function makeMockRoomState(memberships: CallMembershipData[], roomId: string, localAge: number | null = null) {
+    const event = mockRTCEvent(memberships, roomId, localAge);
     return {
         getStateEvents: (_: string, stateKey: string) => {
-            // If localTimestamp is present mockRTCEvent does not recompute
-            // the localTimestamp based on Date.now() when `getStateEvents` is called.
-            const event = mockRTCEvent(memberships, roomId, localAge, localTimestamp);
-
             if (stateKey !== undefined) return event;
             return [event];
         },
     };
 }
 
-export function mockRTCEvent(
-    memberships: CallMembershipData[],
-    roomId: string,
-    /**`localAge` is ignored if localTimestamp is present.*/
-    localAge: number | null,
-    localTimestamp: number | null = null,
-): MatrixEvent {
-    const _localTimestamp = localTimestamp ?? Date.now() - (localAge ?? 10);
-
+export function mockRTCEvent(memberships: CallMembershipData[], roomId: string, localAge: number | null): MatrixEvent {
     return {
         getType: jest.fn().mockReturnValue(EventType.GroupCallMemberPrefix),
         getContent: jest.fn().mockReturnValue({
@@ -70,7 +48,7 @@ export function mockRTCEvent(
         }),
         getSender: jest.fn().mockReturnValue("@mock:user.example"),
         getTs: jest.fn().mockReturnValue(1000),
-        localTimestamp: _localTimestamp,
+        localTimestamp: Date.now() - (localAge ?? 10),
         getRoomId: jest.fn().mockReturnValue(roomId),
         sender: {
             userId: "@mock:user.example",
