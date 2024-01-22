9cb7b47ce29eafe83289ba36b50c17a667223e88
Valere <bill.carson@valrsoft.com>
2024-01-22 14:45:02 +0100

    review: Better doc
    Co-authored-by: Richard van der Hoff <1389908+richvdh@users.noreply.github.com>

--- src/rust-crypto/KeyClaimManager.ts
+++ src/rust-crypto/KeyClaimManager.ts
@@ -75,7 +75,7 @@ export class KeyClaimManager {
         logger.info("Checking for missing Olm sessions");
         // By passing the userId array to rust we transfer ownership of the items to rust, causing
         // them to be invalidated on the JS side as soon as the method is called.
-        // As we haven't created the `userList` let's clone it to not break the caller from re-using it.
+        // As we haven't created the `userList` let's clone the users, to not break the caller from re-using it.
         const claimRequest = await this.olmMachine.getMissingSessions(userList.map((u) => u.clone()));
         if (claimRequest) {
             logger.info("Making /keys/claim request");
