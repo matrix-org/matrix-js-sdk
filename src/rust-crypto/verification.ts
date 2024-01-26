0e71c0371bcdadac697f5d94a1e8f53f96a5f3e1
Hubert Chathi <hubertc@matrix.org>
2024-01-26 11:22:39 -0500

    prettier

--- src/rust-crypto/verification.ts
+++ src/rust-crypto/verification.ts
@@ -465,7 +465,9 @@ abstract class BaseRustVerifer<InnerType extends RustSdkCryptoJs.Qr | RustSdkCry
         super();
 
         this.completionDeferred = defer();
-        inner.registerChangesCallback(async () => { await this.onChangeCallback(); });
+        inner.registerChangesCallback(async () => {
+            await this.onChangeCallback();
+        });
         // stop the runtime complaining if nobody catches a failure
         this.completionDeferred.promise.catch(() => null);
     }
@@ -481,7 +483,7 @@ abstract class BaseRustVerifer<InnerType extends RustSdkCryptoJs.Qr | RustSdkCry
                 new Error(
                     `Verification cancelled by ${
                         cancelInfo.cancelledbyUs() ? "us" : "them"
-                     } with code ${cancelInfo.cancelCode()}: ${cancelInfo.reason()}`,
+                    } with code ${cancelInfo.cancelCode()}: ${cancelInfo.reason()}`,
                 ),
             );
         }
@@ -733,7 +735,9 @@ export class RustSASVerifier extends BaseRustVerifer<RustSdkCryptoJs.Sas> implem
     public replaceInner(inner: RustSdkCryptoJs.Sas): void {
         if (this.inner != inner) {
             this.inner = inner;
-            inner.registerChangesCallback(async () => { await this.onChangeCallback(); });
+            inner.registerChangesCallback(async () => {
+                await this.onChangeCallback();
+            });
             // replaceInner will only get called if we started the verification at the same time as the other side, and we lost
             // the tie breaker.  So we need to re-accept their verification.
             this.sendAccept();
