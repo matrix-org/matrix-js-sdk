895901e677ddb7cf44fef1ce4d045567d6898bb9
Travis Ralston <travisr@element.io>
2024-04-18 16:50:21 -0600

    Further fix tests?

--- src/crypto/crypto.ts
+++ src/crypto/crypto.ts
@@ -41,10 +41,10 @@ if (!TextEncoder) {
 /* eslint-enable @typescript-eslint/no-var-requires */
 
 // @ts-expect-error - we don't have service worker types. See 'fetch' listener in Element Web service worker.
-if (typeof self?.["skipWaiting"] === "function") {
-    crypto = self.crypto;
-    subtleCrypto = self.crypto.subtle ?? self.crypto.webkitSubtle;
-    TextEncoder = self.TextEncoder;
+if (typeof globalThis?.["skipWaiting"] === "function") {
+    crypto = globalThis.crypto;
+    subtleCrypto = globalThis.crypto.subtle ?? globalThis.crypto.webkitSubtle;
+    TextEncoder = globalThis.TextEncoder;
 }
 
 export function setCrypto(_crypto: Crypto): void {
