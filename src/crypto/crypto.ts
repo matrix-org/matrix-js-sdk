529fcc369a6b6f6ab77f004b2952c35f5f2cd1cf
Travis Ralston <travisr@element.io>
2024-04-18 16:29:26 -0600

    Fix tests/unsafe execution

--- src/crypto/crypto.ts
+++ src/crypto/crypto.ts
@@ -41,7 +41,7 @@ if (!TextEncoder) {
 /* eslint-enable @typescript-eslint/no-var-requires */
 
 // @ts-expect-error - we don't have service worker types. See 'fetch' listener in Element Web service worker.
-if (typeof self["skipWaiting"] === "function") {
+if (typeof self?.["skipWaiting"] === "function") {
     crypto = self.crypto;
     subtleCrypto = self.crypto.subtle ?? self.crypto.webkitSubtle;
     TextEncoder = self.TextEncoder;
