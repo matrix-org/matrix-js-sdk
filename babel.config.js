16f127157f282274b09f1da97ed53c0b81d8f126
Bayyr Oorjak <the.bayyr.oorjak@gmail.com>
2024-05-18 14:52:38 +0300

    refactor: add comments
    Signed-off-by: Bayyr Oorjak <the.bayyr.oorjak@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,9 @@ module.exports = {
                 targets: {
                     esmodules: true,
                 },
+                // Used "commonjs" for tests due to https://github.com/matrix-org/matrix-js-sdk/pull/4187#issuecomment-2117342908
+                // and "false" to preserve ESM for the final build to make async
+                // imports work correctly.
                 modules: process.env.NODE_ENV === "test" ? "commonjs" : false,
             },
         ],
