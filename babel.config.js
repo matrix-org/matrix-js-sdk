9a5a5d3cb9dc74a679c38271e08e8b60bbcab28c
Richard van der Hoff <1389908+richvdh@users.noreply.github.com>
2024-05-21 10:52:59 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,7 @@ module.exports = {
                     esmodules: true,
                 },
                 // We want to output ES modules for the final build (mostly to ensure that
-                // async imports work correctly). However, junit doesn't support ES modules very
+                // async imports work correctly). However, jest doesn't support ES modules very
                 // well yet (see https://github.com/jestjs/jest/issues/9430), so we use commonjs
                 // when testing.
                 modules: process.env.NODE_ENV === "test" ? "commonjs" : false,
