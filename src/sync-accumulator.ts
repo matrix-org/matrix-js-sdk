28fd0c0259660588f386eadba68174c2c3b55696
Michael Telatynski <7t3chguy@gmail.com>
2024-11-19 18:02:10 +0000

    Fix Sync Accumulator toJSON putting start timeline state in state_after field
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/sync-accumulator.ts
+++ src/sync-accumulator.ts
@@ -660,13 +660,13 @@ export class SyncAccumulator {
             Object.keys(roomData._currentState).forEach((evType) => {
                 Object.keys(roomData._currentState[evType]).forEach((stateKey) => {
                     let ev = roomData._currentState[evType][stateKey];
-                    if (rollBackState[evType] && rollBackState[evType][stateKey]) {
-                        // use the reverse clobbered event instead.
-                        ev = rollBackState[evType][stateKey];
-                    }
                     // Push to both fields to provide downgrade compatibility in the sync accumulator db
                     // the code will prefer `state_after` if it is present
                     roomJson["org.matrix.msc4222.state_after"]?.events.push(ev);
+                    // Roll the state back to the value at the start of the timeline if it was changed
+                    if (rollBackState[evType] && rollBackState[evType][stateKey]) {
+                        ev = rollBackState[evType][stateKey];
+                    }
                     roomJson.state?.events.push(ev);
                 });
             });
