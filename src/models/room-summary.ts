8b7213be3096d54dd2328f95d58ed5a3696c5aec
David Baker <dbkr@users.noreply.github.com>
2024-11-21 15:14:00 +0000

    add ts extension

--- src/models/room-summary.ts
+++ src/models/room-summary.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { Hero } from "./room";
+import { Hero } from "./room.ts";
 
 export interface IRoomSummary {
     "m.heroes": string[];
