c90ea985c6bbfe412506481b37f7c114c5eb48bc
Stanislav Demydiuk <stas-demydiuk@users.noreply.github.com>
2024-12-05 18:08:41 +0200

    Fix age field check in event echo processing (#3635)
    Co-authored-by: David Baker <dbkr@users.noreply.github.com>

--- src/models/event.ts
+++ src/models/event.ts
@@ -1396,7 +1396,7 @@ export class MatrixEvent extends TypedEventEmitter<MatrixEventEmittedEvents, Mat
             this.emit(MatrixEventEvent.LocalEventIdReplaced, this);
         }
 
-        this.localTimestamp = Date.now() - this.getAge()!;
+        this.localTimestamp = Date.now() - (this.getAge() ?? 0);
     }
 
     /**
