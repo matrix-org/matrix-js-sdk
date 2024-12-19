0e5012df8c65c62e09ea6e8bb6c6f712181bc51f
Michael Telatynski <7t3chguy@gmail.com>
2024-12-19 22:24:25 +0000

    Update src/secret-storage.ts
    Co-authored-by: Richard van der Hoff <1389908+richvdh@users.noreply.github.com>

--- src/secret-storage.ts
+++ src/secret-storage.ts
@@ -209,6 +209,8 @@ export type SecretStorageKey = Assignable<AccountDataEvents, SecretInfo>;
 
 /**
  * Account Data event content type for storing secret-storage-encrypted information.
+ *
+ * See https://spec.matrix.org/v1.13/client-server-api/#msecret_storagev1aes-hmac-sha2-1 
  */
 export interface SecretInfo {
     encrypted: {
