da6a1cd96656790ecde9d82046ac45cfe4a7a822
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2025-02-18 21:13:58 +0530

    convert `ImportRoomKeyStage.LoadKeys` to link in `ImportRoomKeyLoadProgress` stage doc
    Co-authored-by: Richard van der Hoff <1389908+richvdh@users.noreply.github.com>

--- src/crypto-api/index.ts
+++ src/crypto-api/index.ts
@@ -1041,7 +1041,7 @@ export type ImportRoomKeyFetchProgress = {
 export type ImportRoomKeyLoadProgress = {
     /**
      * Indicates the current stage of the import process.
-     * For this type, it should be `ImportRoomKeyStage.LoadKeys`.
+     * For this type, it should be {@link ImportRoomKeyStage.LoadKeys}.
      */
     stage: ImportRoomKeyStage.LoadKeys;
 
