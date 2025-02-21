8df56bb7356925410607134eede9d90656476c14
Timo <toger5@hotmail.de>
2025-02-21 13:17:42 +0100

    remove wait for expect dependency

--- spec/unit/matrixrtc/MembershipManager.spec.ts
+++ spec/unit/matrixrtc/MembershipManager.spec.ts
@@ -18,7 +18,6 @@ limitations under the License.
 */
 
 import { type Mock } from "jest-mock";
-import waitForExpect from "wait-for-expect";
 
 import { EventType, HTTPError, MatrixError, type Room } from "../../../src";
 import { type Focus, type LivekitFocusActive, type SessionMembershipData } from "../../../src/matrixrtc";
@@ -255,14 +254,15 @@ describe("MembershipManager", () => {
                     delayedHandle.reject?.(Error("Server does not support the delayed events API"));
                     expect(client._unstable_sendDelayedStateEvent).toHaveBeenCalledTimes(1);
                 });
-                it("does try to schedule a delayed leave event again if rate limited", () => {
+                it("does try to schedule a delayed leave event again if rate limited", async () => {
                     const delayedHandle = createAsyncHandle(client._unstable_sendDelayedStateEvent as Mock);
                     const manager = new TestMembershipManager({}, room, client, () => undefined);
                     manager.join([focus], focusActive);
                     delayedHandle.reject?.(new HTTPError("rate limited", 429, undefined));
-                    waitForExpect(() => {
-                        expect(client._unstable_sendDelayedStateEvent).toHaveBeenCalledTimes(2);
-                    });
+                    await flushPromises();
+                    jest.advanceTimersByTime(5000);
+                    await flushPromises();
+                    expect(client._unstable_sendDelayedStateEvent).toHaveBeenCalledTimes(2);
                 });
                 it("uses membershipServerSideExpiryTimeout from config", async () => {
                     const manager = new TestMembershipManager(
