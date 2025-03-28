09b786ca24202ae874f0b6d595b4c1e449f84c87
David Baker <dbkr@users.noreply.github.com>
2025-03-28 13:40:22 +0000

    Fix imports

--- src/sync-helpers.ts
+++ src/sync-helpers.ts
@@ -14,7 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { NotificationCountType, type Room, type UnreadNotificationCounts } from "./matrix.ts";
+import { type Room, NotificationCountType } from "./models/room.ts";
+import { type UnreadNotificationCounts } from "./sync-accumulator.ts";
 
 /**
  * Updates the thread notification counts for a room based on the value of
