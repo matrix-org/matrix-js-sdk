dae2f3953abd05145dbf83bb3df258c4fa0d235d
Half-Shot <will@half-shot.uk>
2025-10-02 12:05:19 +0100

    Fix the mistake

--- spec/unit/sync-accumulator.spec.ts
+++ spec/unit/sync-accumulator.spec.ts
@@ -1101,7 +1101,7 @@ describe("SyncAccumulator", function () {
             );
             expect(sa.getJSON().roomsData[Category.Join]["!foo:bar"].msc4354_sticky?.events).toEqual([ev]);
         });
-        it.only("should clear stale sticky events", () => {
+        it("should clear stale sticky events", () => {
             jest.setSystemTime(1000);
             const ev = stickyEvent(1000);
             sa.accumulate(
