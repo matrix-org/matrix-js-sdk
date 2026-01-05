35e6804120bbf54a610081eb7a0f0ff4660c9c99
Valere <bill.carson@valrsoft.com>
2026-01-05 15:46:38 +0100

    Add back deprecated API for compat

--- src/matrixrtc/MatrixRTCSession.ts
+++ src/matrixrtc/MatrixRTCSession.ts
@@ -316,6 +316,20 @@ export class MatrixRTCSession extends TypedEventEmitter<
     }
 
     /**
+     *
+     * @deprecated Use `MatrixRTCSession.sessionMembershipsForSlot` instead.
+     * @returns an empty array.
+     */
+    public static callMembershipsForRoom(
+        room: Pick<Room, "getLiveTimeline" | "roomId" | "hasMembershipState" | "_unstable_getStickyEvents">,
+    ): CallMembership[] {
+        rootLogger.error(
+            `[MatrixRTCSession ${room.roomId}] callMembershipsForRoom is deprecated. Use sessionMembershipsForSlot instead.`,
+        );
+        return [];
+    }
+
+    /**
      * Returns all the call memberships for a room that match the provided `sessionDescription`,
      * oldest first.
      *
