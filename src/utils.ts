6745ddf39579e50441440eea99f69258b632c1e1
Michael Telatynski <7t3chguy@gmail.com>
2026-01-13 11:28:28 +0000

    Iterate
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -226,15 +226,23 @@ export function deepCompare(x: any, y: any): boolean {
     }
 
     // everything else is either an unequal primitive, or an object
-    if (typeof x !== "object") {
+    // XXX: this check has been temporarily tweaked due to issues in the jest test environment,
+    // this will be reverted as part of the migration to vitest
+    if (
+        x.constructor.name !== "Object" &&
+        x.constructor.name !== "RegExp" &&
+        x.constructor.name !== "Date" &&
+        x.constructor.name !== "Array"
+    ) {
         return false;
     }
 
-    // XXX: this fails under jest
-    // // check they are the same type of object
-    // if (x.constructor !== y.constructor || x.prototype !== y.prototype) {
-    //     return false;
-    // }
+    // check they are the same type of object
+    // XXX: this check has been temporarily tweaked due to issues in the jest test environment,
+    // this will be reverted as part of the migration to vitest
+    if (x.prototype !== y.prototype) {
+        return false;
+    }
 
     // special-casing for some special types of object
     if (x instanceof RegExp || x instanceof Date) {
