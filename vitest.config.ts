1c7f6ee1b6c88ec18df27e942dcb0290e3fa5e0f
Michael Telatynski <7t3chguy@gmail.com>
2026-01-13 12:45:43 +0000

    Fix slow test reporter
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,10 +5,28 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE files in the repository root for full details.
 */
 
-import { defineConfig, ViteUserConfig } from "vitest/config";
+import { defineConfig, type ViteUserConfig } from "vitest/config";
+import { type Reporter } from "vitest/reporters";
 import { env } from "process";
 
 const reporters: ViteUserConfig["test"]["reporters"] = [["default"]];
+
+const slowTestReporter: Reporter = {
+    onTestRunEnd(testModules, unhandledErrors, reason) {
+        const tests = testModules
+            .flatMap((m) => Array.from(m.children.allTests()))
+            .filter((test) => test.diagnostic().slow);
+        tests.sort((x, y) => x.diagnostic()?.duration! - y.diagnostic()?.duration!);
+        tests.reverse();
+
+        if (tests.length > 0) {
+            console.warn("Slowest 10 tests:");
+        }
+        for (const t of tests.slice(0, 10)) {
+            console.warn(`${t.module.moduleId} > ${t.fullName}: ${t.diagnostic()?.duration.toFixed(0)}ms`);
+        }
+    },
+};
 
 // if we're running under GHA, enable the GHA & Sonar reporters
 if (env["GITHUB_ACTIONS"] !== undefined) {
@@ -24,7 +42,7 @@ if (env["GITHUB_ACTIONS"] !== undefined) {
 
     // if we're running against the develop branch, also enable the slow test reporter
     if (env["GITHUB_REF"] == "refs/heads/develop") {
-        reporters.push("<rootDir>/spec/slowReporter.cjs");
+        reporters.push(slowTestReporter);
     }
 }
 
