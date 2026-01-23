a26628e9861f8745ad874a509f0ee48cb1eeb5b3
Hugh Nimmo-Smith <hughns@matrix.org>
2026-01-23 13:43:35 +0000

    Fix complication error

--- src/oidc/register.ts
+++ src/oidc/register.ts
@@ -53,6 +53,28 @@ interface OidcRegistrationRequestBody {
 }
 
 /**
+ * The OAuth 2.0 grant types that are defined for Matrix in https://spec.matrix.org/v1.17/client-server-api/#grant-types
+ */
+export enum OAuthGrantType {
+    /**
+     * See https://spec.matrix.org/v1.17/client-server-api/#authorization-code-grant
+     */
+    AuthorizationCode = "authorization_code",
+    /**
+     * https://spec.matrix.org/v1.17/client-server-api/#refresh-token-grant
+     */
+    RefreshToken = "refresh_token",
+    /**
+     * The OAuth 2.0 Device Authorization Grant type identifier as per
+     * https://www.rfc-editor.org/rfc/rfc8628.html#section-7.2 from
+     * [MSC4341](https://github.com/matrix-org/matrix-spec-proposals/pull/4341).
+     *
+     * @experimental Note that this is UNSTABLE and may have breaking changes without notice.
+     */
+    DeviceAuthorization = "urn:ietf:params:oauth:grant-type:device_code",
+}
+
+/**
  * The name "scope" is a misnomer here as it is actually a "grant type".
  *
  * @deprecated use `OAuthGrantType.DeviceAuthorization` instead
@@ -139,25 +161,3 @@ export const registerOidcClient = async (
         }
     }
 };
-
-/**
- * The OAuth 2.0 grant types that are defined for Matrix in https://spec.matrix.org/v1.17/client-server-api/#grant-types
- */
-export enum OAuthGrantType {
-    /**
-     * See https://spec.matrix.org/v1.17/client-server-api/#authorization-code-grant
-     */
-    AuthorizationCode = "authorization_code",
-    /**
-     * https://spec.matrix.org/v1.17/client-server-api/#refresh-token-grant
-     */
-    RefreshToken = "refresh_token",
-    /**
-     * The OAuth 2.0 Device Authorization Grant type identifier as per
-     * https://www.rfc-editor.org/rfc/rfc8628.html#section-7.2 from
-     * [MSC4341](https://github.com/matrix-org/matrix-spec-proposals/pull/4341).
-     *
-     * @experimental Note that this is UNSTABLE and may have breaking changes without notice.
-     */
-    DeviceAuthorization = "urn:ietf:params:oauth:grant-type:device_code",
-}
