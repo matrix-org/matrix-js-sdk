eadfa7306b0ebb24a6ea995904a122d641585c70
Michael Telatynski <7t3chguy@gmail.com>
2026-05-29 14:47:25 +0100

    Revert "Test coverage changes"
    This reverts commit 3b418db94d08a20ea919cf7275fa4ce9973368ed.

--- src/models/room.ts
+++ src/models/room.ts
@@ -920,10 +920,6 @@ export class Room extends ReadReceipt<RoomEmittedEvents, RoomEventHandlerMap> {
         return this.myUserId;
     }
 
-    public foobar(): string {
-        return this.roomId + "foobar";
-    }
-
     /**
      * Gets the "functional members" in this room.
      *
