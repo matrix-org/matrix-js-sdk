75c9ddada3ab034f1e9d87472156392ae491166f
Strac Consulting Engineers Pty Ltd <preminik@preminik.com>
2026-08-01 13:05:28 +1000

    Update MatrixRTCSession.spec.ts

--- spec/unit/matrixrtc/MatrixRTCSession.spec.ts
+++ spec/unit/matrixrtc/MatrixRTCSession.spec.ts
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+
 import { type Mock } from "vitest";
 
 import { type EventTimeline, EventType, KnownMembership, MatrixClient, type Room } from "../../../src";
